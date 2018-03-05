51e684af5209aae901a281fcf85db4d71a203ccc
VinnieScalco <vinnie.scalco@gmail.com>
2018-03-05 17:27:38 -0600

    Fizzbuzz, Strings

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -9,7 +9,28 @@
 */
 int fizzbuzz(int n)
 {
-
+    int counter = 0;
+    for (int i = 0; i <= n; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            printf("fizzbuzz\n");
+        }
+        else if (i % 3 == 0)
+        {
+            printf("fizz\n");
+        }
+        else if (i % 5 == 0)
+        {
+            printf("buzz\n");
+        }
+        else
+        {
+            printf("%d\n", i);
+            counter++;
+        }
+    }
+    return counter;
 }
 
 #ifndef TESTING
--- strings/strings.c
+++ strings/strings.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /*
     Given a character array s, return the number of characters 
     held inside it.
 */
 int string_length(char s[])
 {
-
+    int n = 0;
+    while (s[n] != '\0') {
+        n++;
+    }
+    return n;
 }
-
 /*
     Don't worry about this funky 'char *' going on here. We'll get to
     that shortly. In the meantime, write a function that reverses the 
@@ -20,21 +23,21 @@ int string_length(char s[])
 */
 char *reverse_string(char rv[], char s[])
 {
-
+    int n = string_length(s);
+    for (int i = 0; i < n; i++) {
+    rv[i] = s[n-i-1];
 }
-
+    rv[n] = '\0';
+    return rv;
+}
 #ifndef TESTING
 int main(void)
 {
     char quote1[] = "Don't forget to be awesome";
     char quote2[] = "a man a plan a canal panama";
-
     char rv[512];
-
     printf("The string 'Don't forget to be awesome' has %d characters.\n", string_length(quote1));
     printf("The string 'a man a plan a canal panama' reversed is: '%s'\n", reverse_string(rv, quote2));
-
     return 0;
 }
 #endif
-    
