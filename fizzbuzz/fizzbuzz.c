916e52f3781c16d1716c27c272b1493b4e46be63
Cole Phillips <nphillips78@gmail.com>
2018-03-05 17:02:04 -0500

    fizzzzz

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -9,7 +9,26 @@
 */
 int fizzbuzz(int n)
 {
-
+    int counter = 0;
+    for(int i = 0; i <= n; i++) {
+        if(i % 3 == 0 && i % 5 == 0)
+        {
+            printf("fizzbuzz\n");
+        }
+        else if(i % 3 == 0) 
+        {
+            printf("fizz\n");
+        }
+        else if(i % 5 == 0)
+        {
+            printf("buzz\n");
+        }
+        else
+        {
+            counter++;
+        }
+    }  
+    return counter;
 }
 
 #ifndef TESTING
