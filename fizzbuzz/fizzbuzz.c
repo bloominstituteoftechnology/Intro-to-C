6656e698569293770625d190988fbcc13d0fc8b5
Jason Metten <metten@gmail.com>
2018-03-05 14:20:19 -0600

    Completed Fizzbuzz

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -9,7 +9,26 @@
 */
 int fizzbuzz(int n)
 {
-
+    for (int i = 0; i <= n; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            printf("fizzbuzz\n");
+        }
+        else if (i % 3 == 0)
+        {
+            printf("fizz\n");
+        }
+        else if (i % 5 == 0)
+        {
+            printf("buzz\n");
+        }
+        else
+        {
+            printf("%d\n", i);
+        }
+    }
+    return 0;
 }
 
 #ifndef TESTING
