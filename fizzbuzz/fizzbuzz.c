8c4f959d675037af3db9eabc74c6da9b86483343
Adam Fetters <adamfetterscoder@gmail.com>
2018-03-05 15:06:33 -0500

    Fizzbuzz working properly

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -7,15 +7,44 @@
     by both 3 and 5, print "FizzBuzz". Increment the counter variable
     every time that nothing gets printed and return the counter.
 */
+
+int fizzbuzz(int n);
+
 int fizzbuzz(int n)
 {
+    int counter = 0;
+    for (int i = 1; i < n; ++i)
 
+    {
+        printf("i is equal to %d: ", i);
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            printf("fizzbuzz\n");
+            continue;
+        }
+        if (i % 3 == 0)
+        {
+            printf("fizz\n");
+        }
+        else if (i % 5 == 0)
+        {
+            printf("buzz\n");
+        }
+        else
+        {
+            printf("\n");
+            counter++;
+        }
+    }
+
+    return counter;
 }
 
 #ifndef TESTING
 int main(void)
 {
-    fizzbuzz(20);
+    int finalResult = fizzbuzz(20);
+    printf("The number neither divisible by 3 nor 5 is %d\n", finalResult);
 
     return 0;
 }
