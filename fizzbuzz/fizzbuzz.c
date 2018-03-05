0eb034340821e914a528b1fd97e9ac78ec2dbecc
Patrick Sandoval <patsand24csu@yahoo.com>
2018-03-05 11:02:15 -0800

    solved fizzbuzz

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -7,9 +7,30 @@
     by both 3 and 5, print "FizzBuzz". Increment the counter variable
     every time that nothing gets printed and return the counter.
 */
+int fizzbuzz(int n);
+
 int fizzbuzz(int n)
 {
-
+    for(int i = 1; i < n; ++i)
+    {
+        if(i % 3 == 0 && i % 5 == 0)
+        {
+            printf("fizzbuzz\n");
+        }
+        else if(i % 3 == 0)
+        {
+            printf("fizz\n");
+        }
+        else if(i % 5 == 0)
+        {
+            printf("buzz\n");
+        }
+        else
+        {
+            printf("%d\n", i);
+        }
+    }
+    return 0;
 }
 
 #ifndef TESTING
