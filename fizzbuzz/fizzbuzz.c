4176bb5bb085db9784e5257870b73ad090e8399f
Jesse Hood <jessenoob@gmail.com>
2018-03-05 19:30:04 -0500

    fizzbuzz completed

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -7,16 +7,39 @@
     by both 3 and 5, print "FizzBuzz". Increment the counter variable
     every time that nothing gets printed and return the counter.
 */
+int fizzbuzz(int n);
+
 int fizzbuzz(int n)
 {
-
+    int counter = 0;
+    for (int i = 1; i < n; ++i) 
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            printf("fizzbuzz\n");
+        }
+        else if (i % 3 == 0)
+        {
+            printf("fizz\n");
+        }
+        else if (i % 5 == 0)
+        {
+            printf("buzz\n");
+        }
+        else
+        {
+            printf("\n");
+            counter++;
+        }
+    }
+    return counter;
 }
 
 #ifndef TESTING
 int main(void)
 {
-    fizzbuzz(20);
-
+    int finalResult = fizzbuzz(20);
+    printf("The result is: %d", finalResult);
     return 0;
 }
 #endif
