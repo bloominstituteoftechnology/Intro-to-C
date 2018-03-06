89212a664047fa68e9a6657cd6e5360321509240
Moises Dobarganes <designerexpert@gmail.com>
2018-03-05 19:07:51 -0500

    Quicksort finished, hardest thing I have had to do so far

--- README.md
+++ README.md
@@ -12,8 +12,8 @@ This lab is structured very much like how your introductory JavaScript labs are 
 
   - [x] fizzbuzz
   - [x] strings
-  - [ ] pointers
-  - [ ] quicksort
+  - [x] pointers
+  - [x] quicksort
   - [ ] malloc
   - [ ] structs
   - [ ] callbacks
--- quicksort/quicksort.c
+++ quicksort/quicksort.c
@@ -1,25 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "lib.h" 
+#include "lib.h"
+
+void quickSort();
 
 /*
     Implement the Quicksort algorithm. You'll likely want to re-use the
     `swap` function you implemented in the pointers module (which is why
     the pointers header file is included here).
 */
+int div(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
 void quicksort(int arr[], int low, int high)
 {
-
-}
+    if (low < high)
+    {
+        int divide = div(arr, low, high);
+        quicksort(arr, low, divide - 1);
+        quicksort(arr, divide + 1, high);
+    }
+};
 
 #ifndef TESTING
 int main(void)
 {
     int arr1[] = {100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79};
     int n = sizeof(arr1) / sizeof(arr1[0]);
-    quicksort(arr1, 0, n-1);
+    quicksort(arr1, 0, n - 1);
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         printf("%d ", arr1[i]);
     }
 
