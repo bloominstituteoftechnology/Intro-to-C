fcad289470c7e52b6e6b12778de9a3712d9a87e3
Camila Daniels <camila.daniels926@gmail.com>
2018-04-02 15:40:03 -0500

    Finished fizzbuzz and strings.

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -9,7 +9,19 @@
 */
 int fizzbuzz(int n)
 {
-
+	int counter = 0;
+	for (int i = 0; i < n; i ++) {
+		if (i % 3 == 0 && i % 5 != 0) {
+			printf("Fizz\n");
+		} else if (i % 5 == 0 && i % 3 != 0) {
+			printf("Buzz\n");
+		} else if (i % 5 == 0 && i % 3 == 0) {
+			printf("FizzBuzz\n");
+		} else {
+			counter++;
+		}
+	}
+	return counter;
 }
 
 #ifndef TESTING
