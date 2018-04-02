c1363a36be8c2ee5043fd3c70feb666608addb37
Troy Williams <troyDonwilliams@gmail.com>
2018-04-02 15:21:11 -0500

    strings working

--- strings/strings.c
+++ strings/strings.c
@@ -2,25 +2,36 @@
 #include <stdlib.h>
 
 /*
-    Given a character array s, return the number of characters 
+    Given a character array, s, return the number of characters
     held inside it.
 */
 int string_length(char s[])
 {
-
+  int count = 0;
+  while (s[count] !='\0') {
+    count++;
+  };
+  return count;
 }
 
 /*
     Don't worry about this funky 'char *' going on here. We'll get to
-    that shortly. In the meantime, write a function that reverses the 
-    order of string s and outputs the reversed string to the input 
-    array rv. The rv array will have enough space for the reversed 
+    that shortly. In the meantime, write a function that reverses the
+    order of string s and outputs the reversed string to the input
+    array rv. The rv array will have enough space for the reversed
     string. Don't forget to terminate the reversed string with a null
     character. Return the rv array.
 */
 char *reverse_string(char rv[], char s[])
 {
+  int j = 0;
+  for (int i = string_length(s) - 1; i >= 0; i-- ) {
+    rv[j] = s[i];
+    j = j + 1;
+  }
+  rv[string_length(s)] = '\0';
 
+  return rv;
 }
 
 #ifndef TESTING
@@ -37,4 +48,3 @@ int main(void)
     return 0;
 }
 #endif
-    
