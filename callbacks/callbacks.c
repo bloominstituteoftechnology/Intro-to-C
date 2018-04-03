f11ce630cd1a3233fd76079e787556d9ef847f16
Troy Williams <troyDonwilliams@gmail.com>
2018-04-03 17:19:00 -0500

    callbacks done

--- callbacks/callbacks.c
+++ callbacks/callbacks.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <callbacks.h>
-#include "lib.h" 
+#include "lib.h"
 
 /*
-    Implement the callback that will be passed to your bubble_sort function to sort 
+    Implement the callback that will be passed to your bubble_sort function to sort
     the elements in ascending numerical order.
 */
 int sorted_order(int a, int b)
 {
-
+  return a - b;
 }
 
 /*
@@ -18,18 +18,25 @@ int sorted_order(int a, int b)
 */
 int reverse_order(int a, int b)
 {
-
+  return b - a;
 }
 
 /*
     Receives an array of numbers to be sorted, the number of elements in the array,
     and a callback that specifies the order in which the numbers will be sorted.
-    Your function should allocate memory to hold the sorted data and return the 
-    sorted data. Call the callback as you would any other function. 
+    Your function should allocate memory to hold the sorted data and return the
+    sorted data. Call the callback as you would any other function.
 */
 int *bubble_sort(int *numbers, int n, compare_cb cmp)
 {
-
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - 1; j++) {
+      if (cmp(numbers[j], numbers[j+1]) > 0) {
+        swap(&numbers[j], &numbers[j+1]);
+      }
+    }
+  }
+  return numbers;
 }
 
 #ifndef TESTING
