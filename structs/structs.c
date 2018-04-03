e39e00e72868b85cad8e9d875836ecd881786492
Jacob Gakstatter <jacobgakstatter@gmail.com>
2018-04-03 23:57:07 +0100

    Finished structs, passing tests

--- structs/structs.c
+++ structs/structs.c
@@ -8,7 +8,10 @@
     Person should have the fields `name`, `age`, `height`, and `weight`.
 */
 struct Person {
-
+  char *name;
+  int age;
+  int height;
+  int weight;
 };
 
 /*
@@ -22,7 +25,14 @@ struct Person {
 */
 struct Person *Person_create(char *name, int age, int height, int weight)
 {
+  struct Person *new_person = malloc(sizeof(struct Person));
 
+  new_person->name = string_dup(name);
+  new_person->age = age;
+  new_person->height = height;
+  new_person->weight = weight;
+
+  return new_person;
 }
 
 /*
@@ -31,7 +41,8 @@ struct Person *Person_create(char *name, int age, int height, int weight)
 */
 void Person_destroy(struct Person *who)
 {
-
+  free(who->name);
+  free(who);
 }
 
 #ifndef TESTING
