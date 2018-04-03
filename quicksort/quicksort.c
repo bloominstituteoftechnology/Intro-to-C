8928d13b0f06db92cb3a0aa7ff825ef4cd9db97b
Guelmis Cortina <cguelmis@yahoo.com>
2018-04-03 14:32:13 -0400

    finish quicksort

--- quicksort/quicksort.c
+++ quicksort/quicksort.c
@@ -1,15 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "lib.h" 
+#include "lib.h"
 
 /*
     Implement the Quicksort algorithm. You'll likely want to re-use the
     `swap` function you implemented in the pointers module (which is
     already being included for you in this file).
 */
+int partition (int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
 void quicksort(int arr[], int low, int high)
 {
-
+    if (low < high)
+    {
+        int pi = partition(arr, low, high);
+        quicksort(arr, low, pi - 1);
+        quicksort(arr, pi + 1, high);
+    }
 }
 
 #ifndef TESTING
