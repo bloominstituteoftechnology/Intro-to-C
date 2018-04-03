ba6d00540b6599daf6f3423720212ada6621e20f
Julian Kohlman <juliankohlman@gmail.com>
2018-04-03 12:33:18 -0400

    strings tests passing

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 
 /*
-    Your fizzbuzz implementation should initialize a counter, then
-    iterate n times. If the ith iteration is divisible by 3, print
-    "Fizz". If it's divisible by 5, print "Buzz". If it's divisible
-    by both 3 and 5, print "FizzBuzz". Increment the counter variable
-    every time that nothing gets printed and return the counter.
+  Your fizzbuzz implementation should initialize a counter, then
+  iterate n times. If the ith iteration is divisible by 3, print
+  "Fizz". If it's divisible by 5, print "Buzz". If it's divisible
+  by both 3 and 5, print "FizzBuzz". Increment the counter variable
+  every time that nothing gets printed and return the counter.
 */
 int fizzbuzz(int n)
 {
-
+  int counter = 0;
+  for (int i = 0; i < n; ++i)
+  {
+    if (!(i % 15))
+      printf("FizzBuzz\n");
+    else if (!(i % 3))
+      printf("Fizz\n");
+    else if (!(i % 5))
+      printf("Buzz\n");
+    else
+      counter++;
+  }
+  return counter;
 }
 
 #ifndef TESTING
--- strings/strings.c
+++ strings/strings.c
@@ -1,26 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /*
-    Given a character array s, return the number of characters 
+    Given a character array s, return the number of characters
     held inside it.
 */
 int string_length(char s[])
 {
+    printf("%zu\n", strlen(s) );
 
+    return strlen(s);
 }
 
 /*
     Don't worry about this funky 'char *' going on here. We'll get to
-    that shortly. In the meantime, write a function that reverses the 
-    order of string s and outputs the reversed string to the input 
-    array rv. The rv array will have enough space for the reversed 
+    that shortly. In the meantime, write a function that reverses the
+    order of string s and outputs the reversed string to the input
+    array rv. The rv array will have enough space for the reversed
     string. Don't forget to terminate the reversed string with a null
     character. Return the rv array.
 */
 char *reverse_string(char rv[], char s[])
 {
+    int n = strlen(s);
 
+    for(int i = 0,k = n - 1 ; i < n; i++, k--)
+{
+        char temp = s[k];
+        s[k] = rv[i];
+        rv[i] = temp;
+
+}
+    printf("%lu\n", strlen(rv));
+    rv[n] = '\0';
+    return rv;
 }
 
 #ifndef TESTING
@@ -37,4 +51,4 @@ int main(void)
     return 0;
 }
 #endif
-    
+
