c7950fb5fd182b73da176bc81c461fb0792b6601
Your Name <haywood.d.johnson@gmail.com>
2018-04-04 06:23:43 -0500

    "./structs" ALL TEST PASSED

--- structs/structs.c
+++ structs/structs.c
@@ -7,8 +7,12 @@
     Person type. Don't forget to specify the type of each field. A 
     Person should have the fields `name`, `age`, `height`, and `weight`.
 */
-struct Person {
-
+struct Person
+{
+    char *name;
+    int age;
+    int height;
+    int weight;
 };
 
 /*
@@ -22,7 +26,12 @@ struct Person {
 */
 struct Person *Person_create(char *name, int age, int height, int weight)
 {
-
+    struct Person *new_Person = malloc(sizeof(struct Person));
+    new_Person->name = string_dup(name);
+    new_Person->age = age;
+    new_Person->height = height;
+    new_Person->weight = weight;
+    return new_Person;
 }
 
 /*
@@ -31,7 +40,14 @@ struct Person *Person_create(char *name, int age, int height, int weight)
 */
 void Person_destroy(struct Person *who)
 {
-
+    if (who->name != NULL)
+    {
+        free(who->name);
+    }
+    if (who != NULL)
+    {
+        free(who);
+    }
 }
 
 #ifndef TESTING
