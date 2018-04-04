71019eb66ff56df73fda9c18fb052b7482ced396
Elly <ellyhahn@gmail.com>
2018-04-03 17:16:02 -0700

    add fizzbuzz

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -9,7 +9,30 @@
 */
 int fizzbuzz(int n)
 {
+    int count = 0;
+    int i;
+    for(i=0; i<=n; i++)
+    {
+        if ((i % 3 == 0) && (i % 5 == 0))
+        {
+            printf("FizzBuzz\n");
+        }
+        else if (i % 3 == 0)
+        {
+            printf("Fizz\n");
+        }
+        else if (i % 5 == 0)
+        {
+            printf("Buzz\n");
+        }
+        else
+        { 
+            printf("%d\n", i);
+            count++;
+        }
+    }
 
+    return count;
 }
 
 #ifndef TESTING
