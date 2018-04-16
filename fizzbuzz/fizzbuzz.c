2e3fee938226e75576923cac66ff0c4dfd7d85f1
aceredesign <steven.magadan@hotmail.com>
2018-04-16 14:23:36 -0700

    Finished fizzbuzz

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -9,7 +9,19 @@
 */
 int fizzbuzz(int n)
 {
-
+    int i;
+    for(i = 1; i <= n; i++) {
+        if(i % 3 == 0 && i % 5 == 0) {
+            printf("Fizzbuzz\n");
+        } else if (i % 3 == 0) {
+            printf("Fizz\n");
+        } else if(i % 5 == 0) {
+            printf("Buzz\n");
+        } else {
+            printf("%d\n", i);
+        }
+    }
+    return 0;
 }
 
 #ifndef TESTING
