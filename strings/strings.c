dc4cdbd5103c9a0891ec92f6fe5467cef4b5eead
Dixie Korley <dixiekorley@gmail.com>
2018-04-16 18:27:37 -0400

    Reversing string butnot yet appending

--- strings/strings.c
+++ strings/strings.c
@@ -20,23 +20,26 @@ int string_length(char s[])
     string. Don't forget to terminate the reversed string with a null
     character. Return the rv array.
 */
-// char *reverse_string(char rv[], char s[])
-// {
-
-// }
+char *reverse_string(/*char rv[],*/ char s[])
+{
+    for (int i = strlen(s); i > 0; i--) {
+        printf("%c \n", s[i]);
+    }
+    return 0;
+}
 
 #ifndef TESTING
 int main(void)
 {
-    char quote1[] = "Don't forget to be awesome";
-    // char quote2[] = "a man a plan a canal panama";
+    // char quote1[] = "Don't forget to be awesome";
+    char quote2[] = "a man a plan a canal panama";
 
+    reverse_string(quote2);
     // char rv[512];
 
-    printf("The string 'Don't forget to be awesome' has %d characters.\n", string_length(quote1));
+    // printf("The string 'Don't forget to be awesome' has %d characters.\n", string_length(quote1));
     // printf("The string 'a man a plan a canal panama' reversed is: '%s'\n", reverse_string(rv, quote2));
 
     return 0;
 }
 #endif
-    
