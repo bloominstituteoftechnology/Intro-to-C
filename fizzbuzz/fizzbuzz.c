b618c9f18650e436be9a28a8fbe053f3320746bb
Peter Gray <peter@petergraycreative.com>
2018-04-16 16:50:15 -0500

    Add iteration for FizzBuzz
    Iterate over n time in fizzbuzz

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -9,7 +9,21 @@
 */
 int fizzbuzz(int n)
 {
-
+    int count = 0;
+    for(int i = 0; i <= n; i++) {
+        if (i % 3 == 0) {
+            printf("Fizz\n");
+        }
+        else if (i % 5 == 0) {
+            printf("Buzz\n");
+        }
+        else if (i % 5 == 0 && i % 3 == 0) {
+            printf("FizzBuzz\n");
+        }
+        else {  
+            count = count + 1;
+        }
+    }
 }
 
 #ifndef TESTING
