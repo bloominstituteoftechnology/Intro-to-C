b2be7884d190c7b1a20a9f89901d357e05a51af4
Dixie Korley <dixiekorley@gmail.com>
2018-04-16 18:46:22 -0400

    Strings.c passing

--- strings/strings.c
+++ strings/strings.c
@@ -20,25 +20,26 @@ int string_length(char s[])
     string. Don't forget to terminate the reversed string with a null
     character. Return the rv array.
 */
-char *reverse_string(/*char rv[],*/ char s[])
+char *reverse_string(char rv[], char s[])
 {
-    for (int i = strlen(s); i > 0; i--) {
-        printf("%c \n", s[i]);
+    int a = 0;
+    for (int i = strlen(s)-1; i >= 0; i--) {
+        rv[a] = s[i]; 
+        a++;
     }
-    return 0;
+    return rv;
 }
 
 #ifndef TESTING
 int main(void)
 {
-    // char quote1[] = "Don't forget to be awesome";
+    char quote1[] = "Don't forget to be awesome";
     char quote2[] = "a man a plan a canal panama";
+   char rv[512];
+ 
 
-    reverse_string(quote2);
-    // char rv[512];
-
-    // printf("The string 'Don't forget to be awesome' has %d characters.\n", string_length(quote1));
-    // printf("The string 'a man a plan a canal panama' reversed is: '%s'\n", reverse_string(rv, quote2));
+    printf("The string 'Don't forget to be awesome' has %d characters.\n", string_length(quote1));
+    printf("The string 'a man a plan a canal panama' reversed is: '%s'\n", reverse_string(rv, quote2));
 
     return 0;
 }
