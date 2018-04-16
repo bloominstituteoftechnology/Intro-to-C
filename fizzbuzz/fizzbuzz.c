01689f3f979319589ed65e4979e450bbf52e3eb2
Res Yeg <shaqk702@gmail.com>
2018-04-16 14:42:13 -0700

    fizzbuzz done

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -9,7 +9,17 @@
 */
 int fizzbuzz(int n)
 {
-
+    int i;
+    for(i=1; i<=n; ++i)
+    {
+        if ((i % 3 == 0) && (i % 5 == 0))
+            printf("FizzBuzz\n");
+        if (i % 3 == 0)
+            printf("Fizz\n");
+        if (i % 5 == 0)
+            printf("Buzz\n");
+    }
+    return 0;
 }
 
 #ifndef TESTING
