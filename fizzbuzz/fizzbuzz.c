3fb1c3b8f701e423490a86763a6a8f962757af4e
TERR1E <terrieb.kim@gmail.com>
2018-04-16 11:36:06 -0700

    fizzbuzz complete

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -9,7 +9,20 @@
 */
 int fizzbuzz(int n)
 {
-
+    int counter = 0;
+    int start = 1;
+    for (int i = start; i <= n; i++) {
+        if (i % 3 == 0) {
+            printf("Fizz");
+        } else if (i % 5 == 0) {
+            printf("Buzz");
+        } else if (i % 3 == 0 && i % 5 == 0) {
+            printf("FizzBuzz");
+        } else {
+            counter++;
+        }
+    }
+    return counter;
 }
 
 #ifndef TESTING
