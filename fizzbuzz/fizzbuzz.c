8606b4d4b86750cc1af2a68733379e072ebc3351
Dixie Korley <dixiekorley@gmail.com>
2018-04-16 17:24:47 -0400

    FizzBuzz working and passing tests

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -9,7 +9,23 @@
 */
 int fizzbuzz(int n)
 {
-
+    int count = 0;
+    for (int i = 0; i <= n; i++) {
+        if (i % 15 == 0) {
+            printf("FizzBuzz \n");
+        } 
+        else if (i % 5 == 0) {
+            printf("Buzz \n");
+        }
+         else if (i % 3 == 0) {
+            printf("Fizz \n");
+        }
+        else {
+            count++;
+            printf("%d has count of %d", i, count);
+        }
+    }
+    return count;
 }
 
 #ifndef TESTING
