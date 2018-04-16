e7447bc05537e830635af3da2d27c29c2084d074
Eileen <algorithmsrock>
2018-04-16 15:45:21 -0700

    FizzBuzz and Strings finished

--- strings/strings.c
+++ strings/strings.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /*
     Given a character array s, return the number of characters 
@@ -7,7 +8,11 @@
 */
 int string_length(char s[])
 {
-
+  int string_counter = 0;
+	while (s[string_counter] != '\0') {
+	  string_counter++;
+}
+ return string_counter;
 }
 
 /*
@@ -18,9 +23,18 @@ int string_length(char s[])
     string. Don't forget to terminate the reversed string with a null
     character. Return the rv array.
 */
+
 char *reverse_string(char rv[], char s[])
 {
+  int string_index = 0;
+	int reverse_string_length = string_length(s);
 
+	for (int i = reverse_string_length - 1; i >=0; i--) {
+	  rv[string_index] = s[i];
+		string_index++;
+  }
+   rv[string_index] = '\0';
+	 return rv;
 }
 
 #ifndef TESTING
