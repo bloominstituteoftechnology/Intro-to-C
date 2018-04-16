c0c06262a34367f52163690bfd895c4436734144
John Spraul <kylewibblesmith@gmail.com>
2018-04-16 17:46:35 -0400

    finished fizzbuzz

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -9,7 +9,28 @@
 */
 int fizzbuzz(int n)
 {
-
+    int i;
+    int counter = 0;
+    for (i=0; i<n; i++)
+    {
+        if (i%15 == 0)
+        {
+            printf("FizzBuzz\n");
+        }
+        else if (i%5 == 0)
+        {
+            printf("Buzz\n");
+        }
+        else if (i%3 == 0)
+        {
+            printf("Fizz\n");
+        }
+        else
+        {
+            counter++;
+        }
+    }
+    return counter;
 }
 
 #ifndef TESTING
