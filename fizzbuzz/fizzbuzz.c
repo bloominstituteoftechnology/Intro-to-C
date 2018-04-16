901003ef151a479580d59440e76d1afb77e06b4e
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2018-04-16 17:33:44 -0500

    Fizzbuzz Module
    - in fizzbuzz.c:
    - initialize counter
    - loop over argument number
    - set conditional per instructions
    - print per instructions
    - return counter
    - run tests in CLI (pass)

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -9,7 +9,21 @@
 */
 int fizzbuzz(int n)
 {
-
+    int a = 0;
+    int i;
+    for (i = 1; i <= n; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            printf("FizzBuzz\n");
+        } else if (i % 3 == 0) {
+            printf("Fizz\n");
+        } else if (i % 5 == 0) {
+            printf("Buzz\n");
+        } else {
+            a++;
+            printf("%d\n", a);
+        }
+    }
+    return a;
 }
 
 #ifndef TESTING
