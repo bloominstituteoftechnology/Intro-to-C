5190376141a9193a28bd1913b75b7015f08dd043
Dixie Korley <dixiekorley@gmail.com>
2018-04-16 22:08:05 -0400

    Qucksort returning non sorted for now

--- quicksort/quicksort.c
+++ quicksort/quicksort.c
@@ -1,22 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "lib.h" 
+#include "lib.h"
 
 /*
     Implement the Quicksort algorithm. You'll likely want to re-use the
     `swap` function you implemented in the pointers module (which is
     already being included for you in this file).
 */
-void swap(int* a, int* b)
+
+void swap(int *a, int *b)
 {
-  int temp = *a;
-  *a = *b;
-  *b = temp;
-} 
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (arr[j <= pivot])
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
 
 void quicksort(int arr[], int low, int high)
 {
+    if (low < high)
+    {
+        int p = partition(arr, low, high);
 
+        quicksort(arr, low, p - 1);
+        quicksort(arr, p + 1, high);
+    }
+    // if(low < high) {
+    //     (low + high) / 2;
+    //     swap(&arr[low], &arr[high]);
+    //     x = arr[low];
+    //     y=low+1;
+    //     j = high;
+    //    while(y <= j) {
+    //        while((y <= high) && (arr[y] <= x))
+    //        x++;
+    //        while((j >= low) && (arr[j] > x))
+    //        j--;
+    //        if(y < j)
+    //         swap(&arr[x], &arr[j]);
+    //     }
+
+    //     swap(&arr[low], &arr[j]);
 }
 
 #ifndef TESTING
@@ -24,9 +64,10 @@ int main(void)
 {
     int arr1[] = {100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79};
     int n = sizeof(arr1) / sizeof(arr1[0]);
-    quicksort(arr1, 0, n-1);
+    quicksort(arr1, 0, n - 1);
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         printf("%d ", arr1[i]);
     }
 
