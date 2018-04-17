f9d46bdd3d636a32f4f56e8ab6fe8ee428e24cba
Peter Gray <peter@petergraycreative.com>
2018-04-17 17:49:35 -0500

    Wrote Struct as Sean did in the lecture

--- structs/structs.c
+++ structs/structs.c
@@ -8,7 +8,10 @@
     Person should have the fields `name`, `age`, `height`, and `weight`.
 */
 struct Person {
-
+    char *name;
+    int age;
+    int height;
+    int weight;
 };
 
 /*
@@ -22,7 +25,14 @@ struct Person {
 */
 struct Person *Person_create(char *name, int age, int height, int weight)
 {
+    struct Person *who =  malloc(sizeof(struct Person));
 
+    who->name = string_dup(name);
+    who->age = age;
+    who->height = height;
+    who->weight = weight;
+    
+    return who;
 }
 
 /*
@@ -31,7 +41,8 @@ struct Person *Person_create(char *name, int age, int height, int weight)
 */
 void Person_destroy(struct Person *who)
 {
-
+    free(who->name);
+    free(who);
 }
 
 #ifndef TESTING
