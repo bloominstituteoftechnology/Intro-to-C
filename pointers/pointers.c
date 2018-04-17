f1835aee447f3bd40219c46a731ff6e6b747808c
Eric <erichech@gmail.com>
2018-04-16 21:07:10 -0400

    passing pointers.c

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -8,7 +8,9 @@
 */
 void swap(int* a, int* b)
 {
-
+    int c = *a;
+    *a = *b;
+    *b = c;
 }
 
 /*
@@ -18,7 +20,11 @@ void swap(int* a, int* b)
 */
 int string_length(char *s)
 {
+    char *p = s;
 
+    while(*p != '\0') p++;
+
+    return p - s;
 }
 
 /*
@@ -29,12 +35,18 @@ int string_length(char *s)
 */
 void string_copy(char *x, char *y)
 {
+    int counter = 0;
+    while(y[counter] != '\0') counter++;
 
+    for(int i = 0; i < counter; i++) {
+        x[i] = y[i];
+    }
+    x[counter] = '\0';
 }
 
 /* 
     Compares the character strings m and n and returns negative,
-    0, or positive if n is lexicographically less than, equal to,
+    0, or positive if m is lexicographically less than, equal to,
     or greater than n. To calculate lexicographic difference, find
     the difference between the first characters in m and n that differ.
     
@@ -46,7 +58,24 @@ void string_copy(char *x, char *y)
 */
 int string_compare(char *m, char *n)
 {
+    // int result = 0;
+    // int length = 0;
 
+    // do {
+    //     if(m[length] > n[length]) result++;
+    //     if(m[length] < n[length]) result--;
+    //     length++;
+    // }
+    // while(m[length - 1] == n[length - 1] && m[length - 1] != '\0' && n[length - 1] != '\0');
+
+    // return result;
+
+    do {
+     if (*m < *n) { return -1; }
+     if (*m > *n) { return 1; }
+    } while (*(m++) != '\0' && *(n++) != '\0');
+
+    return 0;
 }
 
 #ifndef TESTING
