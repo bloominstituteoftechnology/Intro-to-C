409b06f7f985c0520f02f8ef0dce203a9ce91c2f
Christopher Beards <beardscm@gmail.com>
2018-04-17 13:26:00 -0400

    Malloc; ALL TESTS PASSED

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -11,7 +11,15 @@
 */
 char *string_dup(char *src)
 {
+  int length = string_length(src);
+  char *str = malloc(length + 1);
 
+  for (int i = 0; i < length; i++)
+  {
+    str[i] = src[i];
+  }
+  str[length] = '\0';
+  return str;
 }
 
 /*
@@ -22,7 +30,13 @@ char *string_dup(char *src)
 */
 void *mem_copy(void *dest, const void *src, int n)
 {
-
+  char *result = dest;
+  char *input = src;
+  for (int i = 0; i < n; i++)
+  {
+    result[i] = input[i];
+  }
+  return result;
 }
 
 #ifndef TESTING
