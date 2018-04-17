177af9ba7109c17f3b2627f58799e69c5c9d8daa
Russell Leon Bates IV <russell.l.bates@gmail.com>
2018-04-17 13:37:52 -0700

    <Leon> Added Quicksort
    *Uses helper function partition().

--- quicksort/quicksort.c
+++ quicksort/quicksort.c
@@ -7,9 +7,29 @@
     `swap` function you implemented in the pointers module (which is
     already being included for you in this file).
 */
+
+int partition(int arr[], int lo, int hi) {
+    int pivot = arr[hi];
+    int i = (lo - 1);
+    int j;
+    for (j=lo; j <= hi-1; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i+1], &arr[hi]);
+    return (i+1);
+};
+
 void quicksort(int arr[], int low, int high)
 {
+    if (low < high) {
+        int pi = partition(arr, low, high);
 
+        quicksort(arr, low, pi-1);
+        quicksort(arr, pi+1, high);
+    }
 }
 
 #ifndef TESTING
