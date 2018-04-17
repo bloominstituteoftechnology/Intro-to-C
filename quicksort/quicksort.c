30f8a0d6fdbffe9621eb12583c0eac7957a3ee6a
Igor Yermak <igor.yermak@gmail.com>
2018-04-17 18:26:01 -0400

    quicksort
    from solution lecture

--- quicksort/quicksort.c
+++ quicksort/quicksort.c
@@ -7,21 +7,33 @@
     `swap` function you implemented in the pointers module (which is
     already being included for you in this file).
 */
-int partition(int arr[], int low, int high) {
-    int pivot = arr[(high / 2)];
-    int j = high;
-    int i = low;
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
 
-    while(arr[j] <= pivot) {
-        i++;
-        swap(&arr[i], &pivot+1);
-        j--;
+    int j = high;
+    int i = low - 1;
+
+    for (int j = low; j <= high-1; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
     }
-    
+
+    swap(&arr[i + 1], &arr[high]);
+    return i + 1;
 }
 void quicksort(int arr[], int low, int high)
 {
-
+    if (low < high)
+    {
+        int index = partition(arr, low, high);
+        quicksort(arr, low, index - 1);
+        quicksort(arr, index + 1, high);
+    }
 }
 
 #ifndef TESTING
