a00177e75025e41a579cd3f584c85b602662501e
astro11 <aquila.strother@asu.edu>
2018-05-21 18:01:43 -0400

    fizzbuzz error

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,21 +10,17 @@
 */
 int fizzbuzz(int n)
 {
-    for (n = 0; n <=100; n++)
-        if (n % 3 == 0 && n % 5 == 0) {
-        printf("FizzBuzz \n", n);
+    for (n = 0; n <= 100; n++) 
+        if (((n % 3) == 0) && ((n % 5) == 0)) {
+        printf("FizzBuzz\n");
+    } else if ((n % 3) == 0) {
+        printf("Fizz\n");
+    } else if ((n % 5) == 0) {
+        printf("Buzz\n");
+    } else {
+        printf("%d\n", n);
     }
-
-    else if (n % 3 == 0) {
-        printf("Fizz \n", n);
-    }
-    
-    else if (n % 5 == 0) {
-        printf("Buzz \n", n);
-    }
-    else {
-        printf("\n", n);
-    }
+        return 0;
 }
 
 #ifndef TESTING
