17061de0aa64f80f5716d938f28755aa857b7f6e
habib1234731 <habibrehman1234731@gmail.com>
2018-05-21 19:26:06 -0400

    quicksort.c completed

--- quicksort/quicksort.c
+++ quicksort/quicksort.c
@@ -8,17 +8,37 @@
 */
 void quicksort(int arr[], int low, int high)
 {
-
+	int pivot, i, j, temp;
+  if(low < high) {
+    pivot = low; 
+    i = low;
+    j = high;
+    while(i < j) {
+      while(arr[i] <= arr[pivot] && i <= high)
+        i++;
+      while(arr[j] > arr[pivot] && j >= low)
+        j--;
+      if(i < j) {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+    temp = arr[j];
+    arr[j] = arr[pivot];
+    arr[pivot] = temp;
+    quicksort(arr, low, j-1);
+    quicksort(arr, j+1, high);
 }
-
+}
 #ifndef TESTING
 int main(void)
 {
     int arr1[] = {100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79};
     int n = sizeof(arr1) / sizeof(arr1[0]);
     quicksort(arr1, 0, n-1);
-
-    for (int i = 0; i < n; i++) {
+	int i;
+    for (i = 0; i < n; i++) {
         printf("%d ", arr1[i]);
     }
 
