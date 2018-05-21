130dcf30666aafe52b99687647ac1e49f2886b5e
YFN Lucci <anderson56ak@gmail.com>
2018-05-21 17:48:24 -0500

    AAndersonWIP

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,22 @@
 */
 int fizzbuzz(int n)
 {
+int main(void)
+{
+    int i;
+    for(i=1; i<=100; ++i)
+    {
+        if (i % 3 == 0)
+            printf("Fizz");
+        if (i % 5 == 0)
+            printf("Buzz");
+        if ((i % 3 != 0) && (i % 5 != 0))
+            printf("number=%d", i);
+        printf("\n");
+    }
 
+    return 0;
+}
 }
 
 #ifndef TESTING
