2c13ec5a827d8eb3ed780fb7ea71009608904d87
Justin Kaseman <justinkaseman@live.com>
2018-05-21 13:33:15 -0700

    Finish fizzbuzz

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -9,8 +9,23 @@
     Don't forget to include newlines '\n' in your printf statements!
 */
 int fizzbuzz(int n)
-{
-
+{   
+    int result = 0;
+    for (int i = 0; i < n; i++) {
+        if ((i % 3 == 0) && (i % 5 == 0)) {
+            printf ("FizzBuzz\n");
+        }
+        else if (i % 3 == 0) {
+            printf ("Fizz\n");
+        }
+        else if (i % 5 == 0) {
+            printf ("Buzz\n");
+        }
+        else {
+            result++;
+        }
+    }
+    return result;
 }
 
 #ifndef TESTING
