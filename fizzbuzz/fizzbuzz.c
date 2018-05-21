d403027bcf33e29a27be1b4e50a14e36d386b0ad
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-05-21 16:01:17 -0400

    fizzbuzz complete
    There are some warnings which I was unable to remove.

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -11,6 +11,28 @@
 int fizzbuzz(int n)
 {
 
+    int counter = 0;
+    for (int num = 0; num <= n; num++) // #QUESTION: Why does it seem to not matter whether num initial is 0 or 1?
+    {
+        if (num % 3 == 0 && num % 5 == 0)
+        {
+            printf("%sFizzBuzz\n"); // %s indicates: "this is a type string"
+        }
+        else if (num % 3 == 0)
+        {
+            printf("%sFizz\n");
+        }
+        else if (num % 5 == 0)
+        {
+            printf("%sBuzz\n");
+        }
+        else
+        {
+            counter = counter + 1;
+            // printf("Counter is: %c\n", counter); // #QUESTION: Why does this not work as expected?
+        };
+    }
+    return counter;
 }
 
 #ifndef TESTING
