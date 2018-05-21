d61549f591e56b90d94ec79fa46b7211a04b932a
crypto-jones <jeremyjones@Macintosh-58b035f2f989.local>
2018-05-21 13:50:16 -0700

    fizzbuzz test passing

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,20 @@
 */
 int fizzbuzz(int n)
 {
-
+    int counter = 0;
+    int i;
+    for (i = 0; i <= n; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+            printf("FizzBuzz\n");
+        else if (i % 3 == 0)
+            printf("Fizz\n");
+        else if (i % 5 == 0)
+            printf("Buzz\n");
+        else
+            counter++;
+    }
+    return counter;
 }
 
 #ifndef TESTING
