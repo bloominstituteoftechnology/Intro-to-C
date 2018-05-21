6da08a7e8818c21b64addc174102e90149c9c4b6
gooseandmegander <meganawilliamson@gmail.com>
2018-05-21 13:12:36 -0700

    fizzbuzz complete

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,22 @@
 */
 int fizzbuzz(int n)
 {
-
+    int count = 0;
+    for (int i = 1; i <= n; ++i) {
+        if (i % 5 == 0 && i % 3 == 0) {
+            printf("%d FizzBuzz\n", i);
+        }
+        else if (i % 3 == 0) {
+            printf("%d Fizz\n", i);
+        }
+        else if (i % 5 == 0) {
+            printf("%d Buzz\n", i);
+        }
+        else {
+            ++count;
+            printf("%d\n", i);
+        }
+    } return count;
 }
 
 #ifndef TESTING
