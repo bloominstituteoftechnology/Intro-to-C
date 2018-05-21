288559538f9f5917dc823c2dbfccb2024e704475
kait-schorr <kaitlynschorr@gmail.com>
2018-05-21 16:22:41 -0500

    Wrote FizzBuzz, All tests passing

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,28 @@
 */
 int fizzbuzz(int n)
 {
-
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if ((i % 3 == 0) && (i % 5 == 0))
+        {
+            printf("FizzBuzz\n");
+        }
+        else if (i % 5 == 0)
+        {
+            printf("Buzz\n");
+        }
+        else if (i % 3 == 0)
+        {
+            printf("Fizz\n");
+        }
+        else
+        {
+            count++;
+            printf("%d\n", count);
+        }
+    }
+    return count;
 }
 
 #ifndef TESTING
