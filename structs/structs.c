7b994e38b6342d5342163ee5101cdb5772fd65a0
kait-schorr <kaitlynschorr@gmail.com>
2018-05-22 17:57:12 -0500

    Added clean up to structs

--- structs/structs.c
+++ structs/structs.c
@@ -39,10 +39,10 @@ struct Person *Person_create(char *name, int age, int height, int weight)
 */
 void Person_destroy(struct Person *who)
 {
-    // if(who->name != NULL)
-    // {
-    // free(who->name);
-    // }
+    if(who->name != NULL)
+    {
+    free(who->name);
+    }
     
     if(who != NULL)
     {
