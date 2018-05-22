f8484b5d365e55e523130b5c3fa135a8a749a755
William <wtpwinberg@gmail.com>
2018-05-22 11:30:27 -0700

    FizzBuzz done

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,29 @@
 */
 int fizzbuzz(int n)
 {
-
+    int i;
+    int counter = 0;
+    for (i = 0; i <= n; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            printf("FizzBuzz\n");
+        }
+        else if (i % 3 == 0)
+        {
+            printf("Fizz\n");
+        }
+        else if (i % 5 == 0)
+        {
+            printf("Buzz\n");
+        }
+        else
+        {
+            printf("%d\n", i);
+            counter++;
+        }
+    }
+    return counter;
 }
 
 #ifndef TESTING
