af9a125672bd2d156b3e1eceb3843b2850d8c2e7
Juber Ahmed <a.juber@hotmail.com>
2018-05-22 13:01:55 -0400

    Completed QuickSort

--- quicksort/quicksort.c
+++ quicksort/quicksort.c
@@ -8,7 +8,28 @@
 */
 void quicksort(int arr[], int low, int high)
 {
-
+	int pivot, i, j, temp;
+  if(low < high) {
+    pivot = low; 
+    i = low;
+    j = high;
+    while(i < j) {
+      while(arr[i] <= arr[pivot] && i <= high)
+        i++;
+      while(arr[j] > arr[pivot] && j >= low)
+        j--;
+      if(i < j) {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+    temp = arr[j];
+    arr[j] = arr[pivot];
+    arr[pivot] = temp;
+    quicksort(arr, low, j-1);
+    quicksort(arr, j+1, high);
+}
 }
 
 #ifndef TESTING
