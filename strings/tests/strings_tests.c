5ccb28d1a57634d62fb4f61859ceb4117875bf1c
Sean Chen <skypemaster007@gmail.com>
2018-05-23 11:27:10 -0700

    Update pointer and string tests

--- strings/tests/strings_tests.c
+++ strings/tests/strings_tests.c
@@ -21,7 +21,10 @@ char *test_reverse_string()
     char str2[] = "a man a plan a canal panama";
     
     mu_assert(check_strings(reverse_string(rv1, str1), "dlrow olleh") == 0, "Your reverse_string function did not correctly reverse a string.");
+    mu_assert(string_length(rv1) == 11, "Your reversed string does not have the same length as the original string.");
+
     mu_assert(check_strings(reverse_string(rv2, str2), "amanap lanac a nalp a nam a") == 0, "Your reverse_string function did not correctly reverse a string.");
+    mu_assert(string_length(rv2) == 27, "Your reversed string does not have the same length as the original string.");
 
     return NULL;
 }
