26a5f4223c1e3ab89f8f094c9016b26de060b9a9
AJ Genung <DragonRydrz@me.com>
2018-05-27 20:13:20 -0400

    removed extra code.  project completed with hangman stretch

--- hangman/hangman.c
+++ hangman/hangman.c
@@ -12,44 +12,11 @@ char HANG_STATES[7][10 * 9] =
 				"             |         |         |         |         |         |         |         |      ",
 				"/*****\\   /*****\\   /*****\\   /*****\\   /*****\\   /*****\\   /*****\\   /*****\\   /*****\\   "};
 
-// void printTurn(int guesses, char *guessedStr, char *charsGuessed)
-// {
-// 	int low = guesses * 10;
-// 	int high = (guesses * 10) + 9;
-// 	// Prints Hangman pic
-// 	for (int i = 0; i <= 6; i++)
-// 	{
-// 		for (int j = low; j <= high; j++)
-// 		{
-// 			printf("%c", HANG_STATES[i][j]);
-// 		}
-// 		printf("\n");
-// 	}
-// 	// Prints current guessedString
-// 	printf("\n%s\n", guessedStr);
-// 	//Prints letters guessed so far
-// 	printf("\nLetters Guessed so far: %s\n", charsGuessed);
-// }
-
-// char *getGuess()
-// {
-// 	char *guess;
-// 	printf("\n\nGuess a letter or the word. Enter quit to leave: ");
-// 	scanf("%s", guess);
-// 	if (strncmp(guess, "quit", 5) == 0)
-// 	{
-// 		printf("\n\nCome play again soon!\n\n\n");
-// 		exit(3);
-// 	}
-// 	return guess;
-// }
-
 int main(int argc, char *argv[])
 {
 	/* Your code here */
 	char *word;
 	char lettersGuessed[100];
-	// char *currentGuess;
 	int guesses = 0;
 
 	if (argc == 1)
@@ -77,12 +44,10 @@ int main(int argc, char *argv[])
 		guessedString[i] = '-';
 	}
 	guessedString[strlen(word)] = '\0';
-	// printf("guessedString: %s\n", guessedString);
 
 	while (guesses < 8)
 	{
 		char *currentGuess;
-		// printf("guessedString 1: %s\n", guessedString);
 
 		int low = guesses * 10;
 		int high = (guesses * 10) + 9;
@@ -98,7 +63,7 @@ int main(int argc, char *argv[])
 		}
 		// Prints current guessedString
 		printf("\n%s\n", guessedString);
-		//Prints letters guessed so far
+		// Prints letters guessed so far
 		printf("\nLetters Guessed so far: %s\n", lettersGuessed);
 
 		printf("\n\nGuess a letter or the word. \nEnter quit to leave: ");
@@ -108,7 +73,6 @@ int main(int argc, char *argv[])
 			printf("\n\nCome play again soon!\n\n\n");
 			exit(3);
 		}
-		// printf("guessedString 2: %s\n", guessedString);
 
 		if (strlen(currentGuess) == 1)
 		{
@@ -131,10 +95,8 @@ int main(int argc, char *argv[])
 		else
 		{
 		}
-		// printf("word: %s\n", word);
 		if (strncmp(word, currentGuess, strlen(word)) == 0 || strncmp(word, guessedString, strlen(word)) == 0)
 		{
-			// printf("word: %s\ncurrentGuess: %s\n", word, currentGuess);
 			printf("\n\nThe word was \"%s\"\nGreat Job! You win!\n\n\n", word);
 			exit(42);
 		}
