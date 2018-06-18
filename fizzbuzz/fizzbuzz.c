cfa835b1efe17214f6f3fcddc8b96fe963c27c45
Mark Wong <fmarkwong@gmail.com>
2018-06-18 13:48:04 -0700

    fizzbuzz

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,29 @@
 */
 int fizzbuzz(int n)
 {
+    int printNothingCounter = 0;
 
+    for (int i = 0; i < n; i++)
+    { 
+        if (i % 15 == 0)
+        {
+            printf("FizzBuzz\n");
+        }
+        else if (i % 3 == 0)
+        {
+            printf("Fizz\n");
+        } 
+        else if (i % 5 == 0)
+        {
+            printf("Buzz\n");
+        } 
+        else 
+        {
+            printNothingCounter++;
+        }
+    }
+
+    return printNothingCounter;
 }
 
 #ifndef TESTING
