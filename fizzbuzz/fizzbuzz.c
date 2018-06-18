7c2ab4aa3ec049277ea02315df32b0c1a1625b95
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-06-18 16:47:55 -0400

    Completed fizzbuzz

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,19 @@
 */
 int fizzbuzz(int n)
 {
-
+    int counter = 0;
+    for (int i = 0; i < n; i++ ) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            printf("Fizzbuzz\n");
+        } else if (i % 5 == 0) {
+            printf("Fizz\n");
+        } else if (i % 3 == 0) {
+            printf("Buzz\n");
+        } else {
+            counter++;
+        }
+    }
+    return counter;
 }
 
 #ifndef TESTING
