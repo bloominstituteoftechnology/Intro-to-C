1e21d90654a3c0573d2b3ae71d44fbec0a02fce6
K <kkhaag@gmail.com>
2018-06-18 15:46:32 -0600

    KH WIP

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,27 @@
 */
 int fizzbuzz(int n)
 {
+    int counter = 0;
 
+    for(int i = 0; i < n; i++)
+    {
+
+    
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+        printf("FizzBuzz");
+    }
+    else if (i % 3 == 0) {
+        printf("Fizz");
+    }
+    
+    else if (i % 5 == 0) {
+        printf("Buzz");
+    }
+    else {
+        counter++;
+    }
+}
+return counter;
 }
 
 #ifndef TESTING
