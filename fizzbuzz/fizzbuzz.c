a4306c1ea2cbd54a3c8d4d64d398f8a0994950da
jaivonmassena <jaivonmassena@gmail.com>
2018-06-18 22:29:45 +0200

    fizzbuzz works test failing fix later

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,20 @@
 */
 int fizzbuzz(int n)
 {
-
+ int i = 1;
+ while(i<= n){
+   if(i % 3 == 0 && i % 5 == 0){
+     printf("FizzBuzz %d\n", i);
+   }
+   else if(i % 5 == 0 ){
+     printf("Buzz %d\n", i);
+   }
+   else if (i % 3 == 0){
+     printf("Fizz %d\n", i);
+   }
+   i++;
+ ;}
+ return i;
 }
 
 #ifndef TESTING
