74a9affa0f6bfae140a1368c44a1b76488e00e99
Cruise Brantley <cruise.brantley@gmail.com>
2018-06-19 16:46:38 -0500

    malloc finished finally

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -34,6 +34,11 @@ char *string_dup(char *src)
 */
 void *mem_copy(void *dest, const void *src, int n)
 {
+    char *dest_copy = dest;
+    const char *src_copy = src;
+    for (int i = 0; i < n; i++)
+        dest_copy[i] = src_copy[i];
+    return dest_copy;
 }
 
 #ifndef TESTING
