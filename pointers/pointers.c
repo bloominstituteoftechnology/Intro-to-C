5179a697c8f17071d8fcc30d7f885c69d5cbe901
Shawn Stewart <s.stewart0220@gmail.com>
2018-06-19 19:07:27 -0400

    Completed pointers

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -6,9 +6,11 @@
     address it's referring to) or the value at the address it's 
     pointing at.
 */
-void swap(int* a, int* b)
+void swap(int *a, int *b)
 {
-
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 /*
@@ -20,7 +22,13 @@ void swap(int* a, int* b)
 */
 int string_length(char *s)
 {
-
+    int i = 0;
+    while (*s != '\0') {
+        s++;
+        i++;
+    }
+    printf("LENGTH: %d\n", i);
+    return i;
 }
 
 /*
@@ -33,7 +41,13 @@ int string_length(char *s)
 */
 void string_copy(char *x, char *y)
 {
+    while (*y != '\0') {
+        *x = *y;
+        x++;
+        y++;
+    }
 
+    *x = '\0';
 }
 
 /* 
@@ -52,7 +66,15 @@ void string_copy(char *x, char *y)
 */
 int string_compare(char *m, char *n)
 {
+    while (*m == *n) {
+        if (*m == '\0') {
+            return 0;
+        }
+        m++;
+        n++;
+    }
 
+    return *m - *n;
 }
 
 #ifndef TESTING
