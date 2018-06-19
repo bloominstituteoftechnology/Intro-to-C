9c3ed800d5ca24b659403de12a5ee1f08c4c80d9
Paul Fang <paulfang@pauls-mbp.home>
2018-06-19 16:08:20 -0700

    Quicksort completed

--- quicksort/quicksort.c
+++ quicksort/quicksort.c
@@ -15,8 +15,28 @@
     
     Do not just use the `qsort` function from the standard library.
 */
+int partition(int *arr, int low, int high)
+{ 
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+
+    swap(&arr[i+1], &arr[high]);
+    return i + 1;
+}
 void quicksort(int *arr, int low, int high)
 {
+    if (low < high) {
+        int index = partition(arr, low, high);
+        quicksort(arr, low, index - 1);
+        quicksort(arr, index + 1, high);
+    }
 
 }
 
