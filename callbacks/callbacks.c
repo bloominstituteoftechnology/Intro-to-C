5fb24a68f7fa2371191f410d0d16621377c7f154
Henry K <mykim1891@hotmail.com>
2018-06-20 01:13:50 -0700

    callbacks.c completed

--- callbacks/callbacks.c
+++ callbacks/callbacks.c
@@ -19,7 +19,6 @@ int sorted_order(int a, int b)
 int reverse_order(int a, int b)
 {
     return b - a;
-
 }
 
 /*
@@ -27,9 +26,42 @@ int reverse_order(int a, int b)
     and a callback that specifies the order in which the numbers will be sorted.
     Your function should allocate memory to hold the sorted data and return the 
     sorted data. Call the callback as you would any other function. 
+
+    Use memcopy in lib.h
 */
+
+//compare_cb 'type'; cmp 'function pointer'
 int *bubble_sort(int *numbers, int n, compare_cb cmp)
 {
+    int swappedTrig = 0;
+    int *saved = malloc(n * sizeof(int));      // pointer to array after sorting
+    mem_copy(saved, numbers, n * sizeof(int)); // copy from numbers to saved.
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (cmp(saved[i], saved[i + 1]) > 0)
+        {
+            swap(&saved[i], &saved[i + 1]); // swap the addresses
+            swappedTrig = 1;
+        }
+        if (i >= n - 2 && swappedTrig != 0)
+        {
+            i = -1;
+            swappedTrig = 0;
+        }
+    }
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n - 1; j++)
+    //     {
+    //         if (cmp(target[j], target[j + 1]) > 0)
+    //         {
+    //             swap(&target[j], &target[j + 1]);
+    //         }
+    //     }
+    // }
+    return saved;
 }
 
 #ifndef TESTING
@@ -39,7 +71,7 @@ int main(void)
     int count = sizeof(arr) / sizeof(arr[0]);
     int *sorted_arr;
 
-    //sorted_arr = bubble_sort(arr, count, sorted_order);
+    // sorted_arr = bubble_sort(arr, count, sorted_order);
     sorted_arr = bubble_sort(arr, count, reverse_order);
 
     for (int i = 0; i < count; i++)
