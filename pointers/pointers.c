b0c3425afc6b540f9c01da1b57de6e15d136cdbe
Justin Hong <jhong2009@gmail.com>
2018-06-20 21:11:41 -0700

    in pointers.c: added alternative solution to swap()

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -8,9 +8,13 @@
 */
 void swap(int *a, int *b)
 {
-    *a += *b;
-    *b = *a - *b;
-    *a -= *b;
+    // *a += *b;
+    // *b = *a - *b;
+    // *a -= *b;
+
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 /*
