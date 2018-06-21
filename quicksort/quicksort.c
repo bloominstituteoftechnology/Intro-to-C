d338db3260c9ff57c0856abb89d3b8a607c3f18c
Jacob Leon Lyerla <umasour@yahoo.com>
2018-06-21 12:40:49 -0700

    Quicksort comments.

--- quicksort/quicksort.c
+++ quicksort/quicksort.c
@@ -15,32 +15,42 @@
     
     Do not just use the `qsort` function from the standard library.
 */
-
+// set up partition function for quicksort
 int partition (int arr[], int low, int high)
 {
-    
+ // set variable for the pivot and set the arr index to the high param
     int pivot = arr[high];
+    // set up a index variable with the value of low -1
     int i = (low - 1);
  
+ // iterate low to high
     for (int j = low; j <= high- 1; j++)
     {
+        // if array at index of j is less than or equal to the pivot
         if (arr[j] <= pivot)
         {
+            // increment i and swap array at i with array at j
             i++;
             swap(&arr[i], &arr[j]);
         }
     }
+    // swap array at i plus 1 with array at index of high
     swap(&arr[i + 1], &arr[high]);
+    // return i plus one
     return (i + 1);
 }
 
+
 void quicksort(int *arr, int low, int high)
 {
+    // if the int of low is less than the int of high
     if (low < high)
     {
+        // set a pi variable, passing in arr, low and high.
         int pi = partition(arr, low, high);
-
+// call quicksort passing in array low and pi minus 1.
         quicksort(arr, low, pi - 1);
+// call quick sorty passing in the array pi + 1 and high
         quicksort(arr,pi + 1, high);
     }
 }
