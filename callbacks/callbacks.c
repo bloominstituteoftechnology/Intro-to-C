639c52a4d0161d536772c4fde63cb3d88362025c
nickbrenn <nbren@tutanota.com>
2018-06-26 12:13:17 -0400

    Finished callbacks and made make tests work by commenting out a line in mainbuild.mk

--- callbacks/callbacks.c
+++ callbacks/callbacks.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "callbacks.h"
-#include "lib.h" 
+#include "lib.h"
 
 /*
     Implement the callback that will be passed to your bubble_sort function to sort 
@@ -9,7 +9,7 @@
 */
 int sorted_order(int a, int b)
 {
-
+    return a > b;
 }
 
 /*
@@ -18,7 +18,7 @@ int sorted_order(int a, int b)
 */
 int reverse_order(int a, int b)
 {
-
+    return a < b;
 }
 
 /*
@@ -29,7 +29,23 @@ int reverse_order(int a, int b)
 */
 int *bubble_sort(int *numbers, int n, compare_cb cmp)
 {
-
+    int *sorting_arr = malloc(n * sizeof(int));
+    mem_copy(sorting_arr, numbers, n * sizeof(int));
+    int i;
+    int sorting = 0;
+    while (sorting == 0)
+    {
+        sorting = 1;
+        for (i = 0; i < n - 1; i++)
+        {
+            if ((*cmp)(sorting_arr[i], sorting_arr[i + 1]))
+            {
+                swap(&sorting_arr[i], &sorting_arr[i + 1]);
+                sorting = 0;
+            }
+        }
+    }
+    return sorting_arr;
 }
 
 #ifndef TESTING
@@ -42,7 +58,8 @@ int main(void)
     //sorted_arr = bubble_sort(arr, count, sorted_order);
     sorted_arr = bubble_sort(arr, count, reverse_order);
 
-    for (int i = 0 ; i < count ; i++) {
+    for (int i = 0; i < count; i++)
+    {
         printf("%d ", sorted_arr[i]);
     }
 
