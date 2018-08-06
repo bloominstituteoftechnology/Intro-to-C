d04804be7a54f09593f5e59d4bc9a188028511dc
mister-corn <code@ronaldlibago.com>
2018-08-06 13:36:34 -0700

    13:26 PDT 6-Aug: Fizzbuzz is done. Tests OK.

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -8,9 +8,51 @@
     every time that nothing gets printed and return the counter.
     Don't forget to include newlines '\n' in your printf statements!
 */
+
 int fizzbuzz(int n)
 {
+    int counter = 0;
 
+    for(int i = 1; i <= n; i++)
+    {
+        if(i % 3 == 0 && i % 5 == 0){
+            printf("FizzBuzz\n");
+        }
+        else if(i % 3 == 0)
+        {
+            printf("Fizz\n");
+        }
+        else if(i % 5 == 0)
+        {
+            printf("Buzz\n");
+        }
+        else {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+int fizzbuzz_alt(int n)
+{
+    for(int i = 1; i <= n; i++)
+    {
+        int printnewline = 0;
+        if(i % 3 == 0)
+        {
+            printf("Fizz");
+            printnewline = 1;
+        }
+        if(i % 5 == 0)
+        {
+            printf("Buzz");
+            printnewline = 1;
+        }
+        if (printnewline == 1) {
+            printf("\n");
+        }
+    }
+    return 0;
 }
 
 #ifndef TESTING
