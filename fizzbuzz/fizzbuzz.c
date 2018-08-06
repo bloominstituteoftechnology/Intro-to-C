077192c2df922f13252a409fc4b7561af4f5a01e
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-08-06 16:28:38 -0400

    FizzBuzz \n added to printf

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -15,19 +15,19 @@ int fizzbuzz(int n)
   {
     if((i%3 == 0) && (i%5 == 0))
     {
-      printf("FizzBuzz");
+      printf("FizzBuzz\n");
     }
     else
     {
       if(i%3 == 0)
       {
-        printf("Fizz");
+        printf("Fizz\n");
       }
       else
       {
         if(i%5 == 0)
         {
-          printf("Buzz");
+          printf("Buzz\n");
         }
         else
         {
