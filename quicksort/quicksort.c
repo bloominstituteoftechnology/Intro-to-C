266fae7ee581bb3c39e55e7f68d35ee8ecc41531
Bradiowave <divebombwalker@gmail.com>
2018-08-06 15:48:17 -0700

    quicksort passes tests

--- quicksort/quicksort.c
+++ quicksort/quicksort.c
@@ -15,15 +15,52 @@
     
     Do not just use the `qsort` function from the standard library.
 */
+
+int descend(int *arr, int pivot, int index) {
+    if (index - 1 == pivot){
+        swap(arr + pivot, arr + index);
+        return index;
+    }
+    else {
+        swap(arr + index, arr + index - 1);
+        return descend(arr, pivot, index - 1);
+    }
+}
+
+
+
 void quicksort(int *arr, int low, int high)
 {
 
+    //make sure our array length > 1
+
+    //create pivot as first value
+
+    //compare each value from low to high
+    //if value is less than pivot, swap with pivot
+
+    //recursively call function from (low, pivot-1) and (pivot+1, high)
+    if (high > low) {
+        int pivot = low;
+        for (int i = low + 1; i <= high; i++) {
+            if (arr[i] < arr[pivot]) {
+                pivot = descend(arr, pivot, i);
+            }
+        }
+
+        quicksort(arr, low, pivot - 1);
+        quicksort(arr, pivot + 1, high);
+    }
 }
 
 #ifndef TESTING
 int main(void)
 {
     int arr1[] = {100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79};
+    {2,1,7,5,0}
+    {1,2,7,5,0}
+    {0,1,2,7,5}
+
     int n = sizeof(arr1) / sizeof(arr1[0]);
     quicksort(arr1, 0, n-1);
 
