005fdf306a5ba114f8db1fca43e1b0bff76776c6
David <AquilaVirtual>
2018-08-06 14:46:08 -0700

    Fizzbuzz done

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -11,7 +11,23 @@
 */
 int fizzbuzz(int n)
 {
+int counter = 0;
+for (int i = 1; i <= n; i++) {
+if (i % 3 == 0) {
+    printf("Fizz\n");
+}
+if  (i % 5 == 0) {
+    printf("Buzz\n");
+}
+  if  (i % 3 == 0  && i % 5 == 0  ) {
+    printf("FizzBuzz\n");
+}
+else {
+    counter++;
+}
+  }
 
+  return counter;
 }
 
 #ifndef TESTING
