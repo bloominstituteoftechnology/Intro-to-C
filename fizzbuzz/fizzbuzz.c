639fec4ed81810b46afbb131fecac10716005066
NandoTheessen <nando.theessen@gmail.com>
2018-08-06 21:56:53 +0200

    Added solution for fizzbuzz

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,22 @@
 */
 int fizzbuzz(int n)
 {
-
+    for(int i = 1; i <= 20; i++)
+    {
+        if(i % 3 == 0)
+        {
+            printf("Fizz\n");
+        }
+        if(i % 5 == 0)
+        {
+            printf("Buzz\n");
+        }
+        if(i % 3 == 0 && i % 5 == 0)
+        {
+            printf("FizzBuzz\n");
+        }
+    };
+    return 0;
 }
 
 #ifndef TESTING
