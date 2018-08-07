92a87389806e748d82e25f99e9ab3779a9310c7a
Danny Vail <danny_vail@outlook.com>
2018-08-07 03:20:46 -0400

    Implemented Person struct

--- structs/structs.c
+++ structs/structs.c
@@ -8,7 +8,10 @@
     Person should have the fields `name`, `age`, `height`, and `weight`.
 */
 struct Person {
-
+  char *name;
+  int age;
+  int height;
+  int weight;
 };
 
 /*
@@ -22,7 +25,13 @@ struct Person {
 */
 struct Person *Person_create(char *name, int age, int height, int weight)
 {
+  struct Person *person = malloc(sizeof(struct Person));
+  person->name = string_dup(name);
+  person->age = age;
+  person->height = height;
+  person->weight = weight;
 
+  return person;
 }
 
 /*
@@ -31,7 +40,11 @@ struct Person *Person_create(char *name, int age, int height, int weight)
 */
 void Person_destroy(struct Person *who)
 {
-
+  if (who->name != NULL)
+    free(who->name);
+  
+  if (who != NULL)
+    free(who);
 }
 
 #ifndef TESTING
