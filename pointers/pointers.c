b3b6d9feccfe1dc307aaf6e8a7a876a94a389999
Christina Kopecky <kopecky12112@gmail.com>
2018-08-06 19:00:41 -0700

    pointers done

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -1,59 +1,10 @@
 #include <stdio.h>
+//define funcs
+void swap(int *a, int *b);
+int string_length(char *s);
+void string_copy(char *x, char *y);
+int string_compare(char *m, char *n);
 
-/*
-    Swaps the integer values being pointed at by a and b. Keep in
-    mind when you need to access a pointer's actual value (the 
-    address it's referring to) or the value at the address it's 
-    pointing at.
-*/
-void swap(int* a, int* b)
-{
-
-}
-
-/*
-    Given a character pointer s, returns the number of characters
-    it is pointing to. Think about how pointer arithmetic can 
-    help you with this.
-    
-    Do not just use the `strlen` function from the standard library.
-*/
-int string_length(char *s)
-{
-
-}
-
-/*
-    Given an empty (NULL) character pointer x and a character pointer y,
-    copies the character contents of y over to x. Again, pointer arithmetic
-    is necessary here. Also, make sure x points to a null character at its 
-    end to terminate it properly. 
-    
-    Do not just use the `strcpy` function from the standard library.
-*/
-void string_copy(char *x, char *y)
-{
-
-}
-
-/* 
-    Compares the character strings m and n and returns negative,
-    0, or positive if n is lexicographically less than, equal to,
-    or greater than n. To calculate lexicographic difference, find
-    the difference between the first characters in m and n that differ.
-    
-    For example, given matching strings, this function should 
-    return 0. Given strings m = "hello world" and n = "goodbye",
-    this function should return a positive value. Given strings
-    m = "aardvark" and n = "zebra", should return a negative
-    value.
-    
-    Do not just use the `strcmp` function from the standard library.
-*/
-int string_compare(char *m, char *n)
-{
-
-}
 
 #ifndef TESTING
 int main(void)
@@ -75,3 +26,73 @@ int main(void)
     return 0;
 }
 #endif
+
+void swap(int *a, int *b){
+    int temp = *a; // temp points to a
+    *a = *b; // a points to b
+    *b = temp; // b points to old a;
+
+}
+
+
+int string_length(char *s){
+    int len = 0;
+    while (*s != '\0'){
+        len ++;
+        s++;
+    }
+    return len;
+}
+
+void string_copy(char *x, char *y){
+    while(*y != '\0'){
+        *x = *y;
+        x++;
+        y++;
+    }
+    *x = '\0';  // x points to new null
+}
+
+/* 
+    Compares the character strings m and n and returns negative,
+    0, or positive if n is lexicographically less than, equal to,
+    or greater than n. To calculate lexicographic difference (ASCII code difference), find
+    the difference between the first characters in m and n that differ.
+    
+    For example, given matching strings, this function should 
+    return 0. Given strings m = "hello world" and n = "goodbye",
+    this function should return a positive value. Given strings
+    m = "aardvark" and n = "zebra", should return a negative
+    value.
+    
+    Do not just use the `strcmp` function from the standard library.
+*/
+int string_compare(char *m, char *n){
+    int i = 0;
+    while(m[i] !='\0'){
+        if(n[i] == '\0'){
+            return 1;
+        }
+        else if(m[i] > n[i]){
+            return 1;
+        }
+        else if(m[i] < n[i]){
+            return -1;
+        }
+        i++;
+    }
+    if (n[i] == '\0'){
+        return 0;
+    }
+    else {
+        return -1;
+    }
+
+  
+
+    
+    
+}
+
+
+
