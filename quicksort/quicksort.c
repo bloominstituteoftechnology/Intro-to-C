8c01a5f0a25860763d3284a2c7ce1028be4435b0
AmyShackles <amyshackles@gmail.com>
2018-08-07 07:56:27 -0700

    Finished quicksort
    - Commented out explanation as to what the quicksort is doing and how.

--- quicksort/quicksort.c
+++ quicksort/quicksort.c
@@ -17,7 +17,47 @@
 */
 void quicksort(int *arr, int low, int high)
 {
+    if (high > low)
+    {
+        int pivot = arr[low], l = low, r = high + 1;
+/* Start with pivot as the value of the first index */
+        while (l < r)
+        {
+/* if the value of l is less than or equal to pivot, increment l -
+ as you increment l and decrement r, the point at which they're the same
+marks the point in the array where you've performed actions on every element */
+            if (arr[l] <= pivot)
+            l++;
+/* If the value of l is greater than the pivot, decrement r and swap
+the element at the address of arr[l] with the element at the address of arr[r] -
+This is comparable to creating a greater-than-pivot array because it's moving
+elements of the array that are greater than the pivot to the right-hand side */
+            else
+                swap(&arr[l], &arr[--r]);
+        }
+/* when you exit the while loop, l and r are both equal -
+since you only increase l if the value of arr[l] is less than or equal to the pivot,
+swap the address of the pivot with the address of decremented l - this gets the
+pivot to where it would be equal to or greater than elements on the left and 
+less than or equal to elements on the right.
 
+If at this point, if --l is 0, the first quicksort does not satisfy the if statement
+and the first element of the array is considered sorted 
+
+Since l and r were equal before the decrement of l, l is now pointing to another
+element in the array than r is - actually, it's pointing to the pivot
+- this pivot is not entered into either recursive quicksort call because it's
+already guaranteed to be between the elements of the less than pivot sort
+and the elements of the greater than pivot sort.
+
+For the second quicksort call, r is passed as the starting index and high as end
+
+If low is greater than high for the second quicksort call, that part of the array
+is already sorted */
+        swap(&arr[--l], &arr[low]);
+        quicksort(arr, low, --l);
+        quicksort(arr, r, high);
+    }
 }
 
 #ifndef TESTING
