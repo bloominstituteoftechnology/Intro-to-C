0e4d63dc6d4d12b361495260188f278e16dc2477
udykas <udykas@gmail.com>
2018-08-07 16:09:03 -0400

    Quicksort passing tests

--- quicksort/quicksort.c
+++ quicksort/quicksort.c
@@ -14,10 +14,56 @@
     parameters to circumvent this issue.
     
     Do not just use the `qsort` function from the standard library.
+
+    Step 1 − Choose the highest index value has pivot
+    Step 2 − Take two variables to point left and right of the list excluding pivot
+    Step 3 − left points to the low index
+    Step 4 − right points to the high
+    Step 5 − while value at left is less than pivot move right
+    Step 6 − while value at right is greater than pivot move left
+    Step 7 − if both step 5 and step 6 does not match swap left and right
+    Step 8 − if left ≥ right, the point where they met is new pivot
+
+    https://www.tutorialspoint.com/data_structures_algorithms/quick_sort_algorithm.htm
 */
+
+int partitionIndex(int *arr, int left, int right, int pivot)
+{
+    int leftPointer = left;
+    int rightPointer = right - 1;
+
+    while (1) {
+        while (arr[leftPointer] < pivot) {
+            leftPointer++;
+        }
+
+        while (rightPointer > 0 && arr[rightPointer] > pivot) {
+            rightPointer--;
+        }
+
+        if (leftPointer >= rightPointer) {
+            break;
+        }
+        else {
+            swap(&arr[leftPointer], &arr[rightPointer]);
+        }
+    }
+
+    swap(&arr[leftPointer], &arr[right]);
+    return leftPointer;
+}
+
 void quicksort(int *arr, int low, int high)
 {
-
+    if (high - low <= 0) {
+        return;
+    }
+    else {
+        int pivot = arr[high];
+        int index = partitionIndex(arr, low, high, pivot);
+        quicksort(arr, low, index - 1);
+        quicksort(arr, index + 1, high);
+    }
 }
 
 #ifndef TESTING
