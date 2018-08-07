91178bbae4b283838f3b03b944ff3e5f0821aec7
David <AquilaVirtual>
2018-08-07 15:22:18 -0700

    malloc done

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -13,7 +13,13 @@
 */
 char *string_dup(char *src)
 {
-
+  int length = string_length(src);
+  char *str = malloc(length + 1);
+  for (int i = 0; i <= length; i++)
+  {
+    *(str + i) = *(src + i);
+  }
+  return str;
 }
 
 /*
@@ -26,6 +32,14 @@ char *string_dup(char *src)
 */
 void *mem_copy(void *dest, const void *src, int n)
 {
+
+  char *csrc = (char *)src;
+  char *cdest = (char *)dest;
+  for (int i = 0; i < n; i++)
+  {
+    *(cdest++) = *(csrc++);
+  }
+  return cdest;
 
 }
 
