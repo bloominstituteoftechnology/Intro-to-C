f313fc9a8a55bc8c8e1de807643b4f5067d675c9
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-08-07 14:02:24 -0700

    malloc done

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -13,7 +13,17 @@
 */
 char *string_dup(char *src)
 {
+  char *dup_string = malloc(string_length(src) + 1);
+  char *temp = dup_string;
 
+  while (*src != '\0') {
+    *temp = *src;
+    temp++;
+    src++;
+  }
+  *temp = '\0';
+
+  return dup_string;
 }
 
 /*
@@ -26,7 +36,14 @@ char *string_dup(char *src)
 */
 void *mem_copy(void *dest, const void *src, int n)
 {
+  char *copy_src = (char *) src;
+  char *copy_dest = (char *) dest;
 
+  for (int i = 0; i < n; i++) {
+    *(copy_dest + i) = *(copy_src + i);
+  }
+
+  return dest;
 }
 
 #ifndef TESTING
