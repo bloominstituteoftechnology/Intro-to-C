4ef4e8faea9124eb906422b9a991654020677e7c
sagardesai90 <saagardesai@gmail.com>
2018-08-07 17:00:52 -0500

    malloc

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -13,7 +13,13 @@
 */
 char *string_dup(char *src)
 {
-
+    int i;
+    int size = strlen(src);
+    char *temp = malloc(size + 1);
+    for (i = 0; i <= size; i++) {
+        temp[i] = src[i];
+    }
+    return temp;
 }
 
 /*
@@ -26,7 +32,12 @@ char *string_dup(char *src)
 */
 void *mem_copy(void *dest, const void *src, int n)
 {
-
+    char *pug = src;
+    char *cat = dest;
+    int i ;
+    for (i = 0; i < n; i++) {
+        pug[i] = cat[i];
+    }
 }
 
 #ifndef TESTING
