d846b8bdf3c3a2a432654f53c13e33fa0d6c6a5c
derrick-mei <derrick.mei.168@gmail.com>
2018-08-06 19:15:46 -0500

    done: strings.c

--- strings/strings.c
+++ strings/strings.c
@@ -9,7 +9,12 @@
 */
 int string_length(char s[])
 {
-
+    int counter = 0;
+    while (s[counter])
+    {
+        counter++;
+    }
+    return counter;
 }
 
 /*
@@ -20,7 +25,14 @@ int string_length(char s[])
 */
 char *reverse_string(char rv[], char s[])
 {
-
+    int lengthS = string_length(s);
+    for (int i = 0; i < string_length(s); i++)
+    {
+        rv[i] = s[lengthS - 1];
+        lengthS -= 1;
+    }
+    rv[string_length(s)] = '\0';
+    return rv;
 }
 
 #ifndef TESTING
@@ -37,4 +49,3 @@ int main(void)
     return 0;
 }
 #endif
-    
