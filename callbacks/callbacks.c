fe0cb98a5ad8a989d542920515500c9da9d7e297
JSNavey <jsnavey@gmail.com>
2018-08-08 20:40:17 -0400

    pass callback

--- callbacks/callbacks.c
+++ callbacks/callbacks.c
@@ -9,7 +9,7 @@
 */
 int sorted_order(int a, int b)
 {
-
+    return a > b;
 }
 
 /*
@@ -18,7 +18,7 @@ int sorted_order(int a, int b)
 */
 int reverse_order(int a, int b)
 {
-
+    return a < b;
 }
 
 /*
@@ -29,7 +29,20 @@ int reverse_order(int a, int b)
 */
 int *bubble_sort(int *numbers, int n, compare_cb cmp)
 {
+    int *sorted = malloc(n * sizeof(int));
+    mem_copy(sorted, numbers, n * sizeof(int));
 
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n - 1; j++)
+        {
+            if(cmp(sorted[j], sorted[j + 1]))
+            {
+                swap(&sorted[j], &sorted[j + 1]);
+            }
+        }
+    }
+    return sorted;
 }
 
 #ifndef TESTING
