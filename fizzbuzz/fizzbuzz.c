358336ba82475fa76896d0aa30bd7d8a8ea287ba
april <martinezapril56@yahoo.com>
2018-08-13 19:24:40 -0400

    commit

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,23 @@
 */
 int fizzbuzz(int n)
 {
-
+//looping thru 1 to 100 incrementing by 1
+for(int i=1; i <=100; ++i){
+    if (i > 0){
+        //if i is didvided by 3 and leaves no remainder
+        //and i is divided by 5 with no remainer
+        //print FizzBuzz digits newline
+    if((i % 3 == 0) && (i % 5 == 0)){
+        printf("FizzBuzz %d\n", i);
+        //if you go thru loop and find numbers divivded by 5 with no remainder
+        //print Buzz
+    }else if(i % 5 == 0){
+        printf("Buzz %d\n", i);     }
+    }else if (i % 3 == 0){
+        printf("Fizz %d\n", i);
+        
+    }
+}
 }
 
 #ifndef TESTING
