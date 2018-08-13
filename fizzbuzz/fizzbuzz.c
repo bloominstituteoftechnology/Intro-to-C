90353a51a2fa9098045a8f31379bcd8de2461e32
Herbert Hodgson <herberthodgson@hotmail.com>
2018-08-13 16:09:50 -0400

    fizzbuzz function implemented with all tests passing.

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,27 @@
 */
 int fizzbuzz(int n)
 {
-
+    int counter = 0;            //initialize a counter
+    for (int i = 0; i < n; i++) //iterate n times
+    {
+        if (i % 3 == 0) // if the ith iteration is divisible by 3, print "Fizz"
+        {
+            printf("Fizz\n");
+        }
+        else if (i % 5 == 0) // if the ith iteration is divisible by 5, print "Fizz"
+        {
+            printf("Buzz\n");
+        }
+        else if (i % 3 == 0 && i % 5 == 0) // if the ith iteration is divisible by 3 and 5, print "FizzBuzz"
+        {
+            printf("FizzBuzz\n");
+        }
+        else // if nothing gets printed (ie. no criteria met), increment counter
+        {
+            counter++;
+        }
+    }
+    return counter; // return the counter
 }
 
 #ifndef TESTING
