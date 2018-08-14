793700305716e56b8a9bebcd4ec0fadaf6158e62
maribelcuales <maribelcuales@gmail.com>
2018-08-15 06:47:27 +0800

    added starter code for fizzbuzz

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,17 @@
 */
 int fizzbuzz(int n)
 {
-
+    for (int i = 0; i < nlen(n); i++ ) {
+        if (i % 3 == 0) {
+            printf('Fizz\n');
+        } else if (i % 5 == 0) {
+            printf('Buzz\n');
+        } else if (i % 3 == 0 && i % 5 == 0) {
+            printf('FizzBuzz\n');
+        } else (i += 1) {
+            return i;
+        }
+    }
 }
 
 #ifndef TESTING
