7316ae73883a81210930d70298e466f00b9ed810
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-08-13 22:24:37 -0700

    docs: add comments to fizzbuzz.c

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -8,30 +8,30 @@
     every time that nothing gets printed and return the counter.
     Don't forget to include newlines '\n' in your printf statements!
 */
-int fizzbuzz(int n)
+int fizzbuzz(int n) // fizzbuzz function receives int n as an argument, returns int
 {
-    int counter = 0;
+    int counter = 0; // initialize counter
 
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= n; i++) // for loop to interate n times
     {
-        if (i % 3 == 0 && i % 5 != 0)
+        if (i % 3 == 0 && i % 5 != 0) // displays "Fizz" if the ith number is a multiple of 3 but not of 5
         {
             printf("Fizz\n");
         }
-        else if (i % 3 != 0 && i % 5 == 0)
+        else if (i % 3 != 0 && i % 5 == 0) // displays "Buzz" if the ith number is a multiple of 5 but not of 3
         {
             printf("Buzz\n");
         }
-        else if (i % 3 == 0 && i % 5 == 0)
+        else if (i % 3 == 0 && i % 5 == 0) // displays "FizzBuzz" if the ith number is a multiple of 3 and 5
         {
             printf("FizzBuzz\n");
         }
         else
         {
-            counter++;
+            counter++; // add 1 to the counter at each iteration
         }
     }
-    return counter;
+    return counter; // return counter after the  iteration is finished
 }
 
 #ifndef TESTING
