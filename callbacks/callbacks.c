10415f0dfa1b4048a7c09b7bee807a084b5d472c
Tramane Hall <tramanedhall@gmail.com>
2018-08-15 18:27:53 -0400

    commented callbacks

--- callbacks/callbacks.c
+++ callbacks/callbacks.c
@@ -9,7 +9,14 @@
 */
 int sorted_order(int a, int b)
 {
-
+    /* Given two elements in the array, if the first is greater than the second,
+    the two elements are in descending order and need to be swapped, so return 1
+    else, return 0 */
+    if (a > b)
+    {
+        return 1;
+    }
+    return 0;
 }
 
 /*
@@ -18,7 +25,14 @@ int sorted_order(int a, int b)
 */
 int reverse_order(int a, int b)
 {
-
+    /* Given two elements in the array, if the second is greater than the first,
+    the two elements are in ascending order and need to be swapped, so return 1
+    else, return 0 */
+    if (a < b)
+    {
+        return 1;
+    }
+    return 0;
 }
 
 /*
@@ -29,8 +43,31 @@ int reverse_order(int a, int b)
 */
 int *bubble_sort(int *numbers, int n, compare_cb cmp)
 {
+    /* Create an int pointer equal to the value of the return of mem_copy,
+    which takes a void pointer to a destination array, the pointer of a source array,
+    and the number of bytes to be copied and copies the source array into the destination array.
+    int *sort is now a pointer to a copy of the numbers array. */
 
+    int *sort = mem_copy(malloc(n * sizeof(int)), numbers, n * sizeof(int));
+    /* Create a nested loop and on every loop, invoke the callback,
+        passing in the value of sort[j] and the value of sort[j + 1]
+        if the return from the callback is greater than 1 (which means the elements are out of order)
+        swap the address of sort[j] with the address of sort[j+1] */
+
+        for (int i = 0; i <= n; i++)
+        {
+            for (int j = 0; j < n - 1; j++)
+            {
+               if (cmp(sort[j], sort[j + 1]))
+               {
+                   swap(&sort[j], &sort[j + 1]);
+               }
+            }
+        }
+    /* Return the now sorted array */
+    return sort;
 }
+
 
 #ifndef TESTING
 int main(void)
