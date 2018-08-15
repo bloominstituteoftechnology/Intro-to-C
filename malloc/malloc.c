59d1bc5d05761fca47a8114fa552b28001556ce3
Herbert Hodgson <herberthodgson@hotmail.com>
2018-08-14 23:21:17 -0400

    malloc.c commented and passing tests

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -13,7 +13,17 @@
 */
 char *string_dup(char *src)
 {
+    int length = string_length(src); // length of the input string
+    char *str = malloc(length + 1);  // allocating memory for the duplicate string using malloc (one byte per character in string + 1 for the null character)
 
+    for (int i = 0; i < length; i++)
+    {
+        *(str + i) = *(src + i); // copying the string into the allocated memory
+    }
+
+    *(str + length) = '\0'; // assign null character after the string to signify the end
+
+    return str; // return a pointer to the allocated memory
 }
 
 /*
@@ -26,7 +36,14 @@ char *string_dup(char *src)
 */
 void *mem_copy(void *dest, const void *src, int n)
 {
+    //casting the input pointers to char pointers before performing the copyting
+    char *csrc = (char *)src;
+    char *cdest = (char *)dest;
 
+    for (int i = 0; i < n; i++)
+    {
+        *(cdest + i) = *(csrc + i); // copying its contents to the destination pointer
+    }
 }
 
 #ifndef TESTING
@@ -40,12 +57,13 @@ int main(void)
     int numbers[] = {100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79};
     int n = sizeof(numbers) / sizeof(numbers[0]);
     int *target = malloc(n * sizeof(int));
-    
+
     mem_copy(target, numbers, n * sizeof(int));
 
     printf("Copied array: ");
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         printf("%d ", target[i]);
     }
 
