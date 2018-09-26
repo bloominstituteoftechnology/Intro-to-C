0cc2c4f048a85994873e47fca9f5f1c7dcb35005
Bhavik Ravani <ravanibhavik@gmail.com>
2018-09-26 00:29:29 -0400

    Completed strings.

--- strings/strings.c
+++ strings/strings.c
@@ -9,7 +9,11 @@
 */
 int string_length(char s[])
 {
-
+    int count = 0;
+    while (s[count] != '\0') {
+        count ++;
+    }
+    return count;
 }
 
 /*
@@ -20,7 +24,13 @@ int string_length(char s[])
 */
 char *reverse_string(char rv[], char s[])
 {
-
+    int count = 0;
+    for (int j = string_length(s) - 1; j >= 0; j--) {
+        rv[count] = s[j];
+        count++;
+    }
+    rv[count] = '\0';
+    return rv;
 }
 
 #ifndef TESTING
