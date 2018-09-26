3c6856ffb9f588d48a4d524e18b8c93fffacfd11
Charlie <ccariste@gmail.com>
2018-09-25 20:33:19 -0700

    CRC: Fizzbuzz done

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,25 @@
 */
 int fizzbuzz(int n)
 {
+    int counter = 0;
+    for (int i = 1; i <= n; i++) {
+        printf("Current Number%d\n", i);
+        if (i % 3 == 0 && i % 5 == 0) {
+            printf("FizzBuzz\n");
+        } 
+        else if (i % 3 == 0) {
+            printf("Fizz\n");
+        }
 
+        else if (i % 5 == 0) {
+            printf("Buzz\n");
+        }
+        else {
+            counter++;
+            printf("Counter: %d\n", counter);
+        }
+    }
+    return counter;
 }
 
 #ifndef TESTING
