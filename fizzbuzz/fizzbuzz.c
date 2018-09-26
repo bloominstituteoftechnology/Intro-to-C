245c5fc77fba2e8381ccf5eec17e6a239345cd8e
Bhavik Ravani <ravanibhavik@gmail.com>
2018-09-25 23:51:37 -0400

    Initial commit - fizzbuzz

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,22 @@
 */
 int fizzbuzz(int n)
 {
-
+    int value = 0;
+    int count = n;
+    while (count > 0) {
+        if (n % 3 == 0 && n % 5 == 0) {
+            printf("FizzBuzz");
+        } else if (n % 3 == 0) {
+            printf("Fizz");
+        } else if (n % 5 == 0) {
+            printf("Buzz");
+        } else {
+            value += 1;
+        }
+        count -= 1;
+        printf("%d", count);
+    }
+    return value;
 }
 
 #ifndef TESTING
