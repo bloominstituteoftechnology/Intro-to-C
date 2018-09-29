355e23aa7fcaf63fb489c614d15db6c11ad00e9b
Alexander <alexander.botello@g.austincc.edu>
2018-09-29 10:28:52 -0500

    Complete quicksort module

--- quicksort/quicksort.c
+++ quicksort/quicksort.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "lib.h" 
+#include "lib.h"
 
 /*
     Implement the Quicksort algorithm. You'll likely want to re-use the
@@ -7,17 +7,36 @@
     already being included for you in this file).
 
     The `low` and `high` parameters indicate the lowest and highest indices
-    of the array that is getting passed in. This is necessary because the 
+    of the array that is getting passed in. This is necessary because the
     function is being passed a pointer to the array, not the contents of the
-    array. We can't easily figure out the length of the array through the 
+    array. We can't easily figure out the length of the array through the
     pointer, so the function receives the highest and lowest indices as
     parameters to circumvent this issue.
-    
+
     Do not just use the `qsort` function from the standard library.
 */
+int getPartitionIndex(int *arr, int low, int high)
+{
+    int pivot = arr[high];
+    int i = low;
+
+    for (int k = low; k < high; k++)
+    {
+        if (arr[k] <= pivot) {
+            swap(&arr[k], &arr[i]);
+            i++;
+        }
+    }
+    swap(&arr[i], &arr[high]);
+    return i;
+}
 void quicksort(int *arr, int low, int high)
 {
-
+    if (low < high) {
+        int index = getPartitionIndex(arr, low, high);
+        quicksort(arr, low, index-1);
+        quicksort(arr, index+1, high);
+    }
 }
 
 #ifndef TESTING
