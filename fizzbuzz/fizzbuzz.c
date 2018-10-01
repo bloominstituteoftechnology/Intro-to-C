8fe890bbb3a51a7e2ea139dfc4341af5cd3dab78
TangledTessellations <mcevoya@oregonstate.edu>
2018-10-01 12:33:23 -0700

    Complete fizzbuzz

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,26 @@
 */
 int fizzbuzz(int n)
 {
+    int counter = 0;
 
+    for(int i = 0; i < n; i++)
+    {
+        if(i % 15 == 0)
+            printf("FizzBuzz\n");
+        else if (i % 5 == 0)
+            printf("Buzz\n");
+        else if (i % 3 == 0)
+            printf("Fizz\n");
+        else
+        {
+            counter++;
+            // printf("i: %d\n", i);
+            // printf("Counter incremented to: %d\n", counter);
+        }
+  
+    }
+
+    return counter;
 }
 
 #ifndef TESTING
