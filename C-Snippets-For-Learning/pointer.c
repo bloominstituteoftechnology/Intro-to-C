dc23151e12d5dc57c85dccd83bd52d38d525bb43
darthMinimus <irishseanvaldivia@gmail.com>
2018-10-01 13:07:43 -0500

    Commenting everywhere to improve understanding.

--- C-Snippets-For-Learning/pointer.c
+++ C-Snippets-For-Learning/pointer.c
@@ -2,8 +2,11 @@
 
 int main(void)
 {
+    // declare a pointer to the string "I'm a string!"
     char *str = "I'm a string!";
+    // print out the value of the actual pointer, or the address of the string
     printf("Address of string(value of pointer): %p\n", str);
+    // print out the value of the string
     printf("Actual value of string: %s\n", str);
 }
 
