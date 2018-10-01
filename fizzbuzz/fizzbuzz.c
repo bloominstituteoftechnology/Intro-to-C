6c2adfce3ecfa81ef2ac102ae3bc3c906cff05dd
Lonnie Davis <ldavis0866@gmail.com>
2018-10-01 14:59:06 -0500

    Initial commit with fizzbuzz complete

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,6 +10,24 @@
 */
 int fizzbuzz(int n)
 {
+    int count = 0;
+    for(int i = 0; i < n; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            printf("FizzBuzz");
+        }
+        else if (i % 5 == 0) {
+            printf("Buzz");
+        }
+        else if (i % 3 == 0) {
+            printf("Fizz");
+        }
+        else {
+            count++;
+        }
+
+        printf("\n");
+    }
+    return count;
 
 }
 
