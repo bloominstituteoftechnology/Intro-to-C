a33b1b8e9e9e2de42bf15f6409366d5675cf68e1
snowcoding <vimalshah77@gmail.com>
2018-10-02 14:57:39 -0700

    Cleaner code for FizzBuzz

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -12,21 +12,10 @@ int fizzbuzz(int n)
 {
     int cnt = 0;
     for (int i=0; i<n;i++){
-        if ( (i % 3 == 0) && (i % 5 == 0) )
-        {
-            printf("FizzBuzz\n");
-        }
-        else if (i % 3 == 0)
-        {
-            printf("Fizz\n");
-        }
-        else if (i % 5 == 0)
-        {
-            printf("Buzz\n");
-        }else
-        {
-            cnt++;
-        }
+        if ( (i % 3 == 0) && (i % 5 == 0) ) printf("FizzBuzz\n");
+        else if (i % 3 == 0) printf("Fizz\n");
+        else if (i % 5 == 0) printf("Buzz\n");
+        else cnt++;
     }
     return cnt;
 }
