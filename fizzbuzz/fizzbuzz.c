b60ece9f9cd6da0c45f2a2a5124ee2577fa85f3b
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-10-02 16:09:50 -0400

    Init, FizzBuzz done

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,27 @@
 */
 int fizzbuzz(int n)
 {
-
+    int start = 0;
+    for (int i = 1; 1 < n; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            printf("FizzBuzz\n");
+        }
+        else if (i % 3 == 0)
+        {
+            printf("Fizz\n");
+        }
+        else if (i % 5 == 0)
+        {
+            printf("Buzz\n");
+        }
+        else
+        {
+            start++;
+        }
+    }
+    return start;
 }
 
 #ifndef TESTING
