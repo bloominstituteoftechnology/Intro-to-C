4e4980fe1857a6bd22f84953d45f8a416c49f53e
Flintbean <ajmerando1@yahoo.com>
2018-10-02 16:02:35 -0700

    Pointers done

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -1,96 +1,155 @@
 #include <stdio.h>
 
 /*
-    Swaps the integer values being pointed at by a and b. Keep in
-    mind when you need to access a pointer's actual value (the 
-    address it's referring to) or the value at the address it's 
-    pointing at.
+	Swaps the integer values being pointed at by a and b. Keep in
+	mind when you need to access a pointer's actual value (the
+	address it's referring to) or the value at the address it's
+	pointing at.
 */
 void swap(int* a, int* b)
 {
+	int aVal = *a;
+	int bVal = *b;
 
+	*a = bVal;
+	*b = aVal;
 }
 
 /*
-    Searches the input string `str` for the first instance of the 
-    character `c` (an unsigned char). This function returns a pointer
-    that points to the first instance of the character `c` in the
-    input string `str`.
+	Searches the input string `str` for the first instance of the
+	character `c` (an unsigned char). This function returns a pointer
+	that points to the first instance of the character `c` in the
+	input string `str`.
 
-    Do not use the `strchr` function from the standard library.
+	Do not use the `strchr` function from the standard library.
 */
 char *find_char(char *str, int c)
 {
-
+	while (*str != '\0') {
+		if (*str == c) {
+			return str;
+		}
+		else {
+			str++;
+		}
+	}
+	return NULL;
 }
 
 /*
-    Given an empty (NULL) character pointer x and a character pointer y,
-    copies the character contents of y over to x. Again, pointer arithmetic
-    is necessary here. Also, make sure x points to a null character at its 
-    end to terminate it properly. 
-    
-    Do not just use the `strcpy` function from the standard library.
+	Given an empty (NULL) character pointer x and a character pointer y,
+	copies the character contents of y over to x. Again, pointer arithmetic
+	is necessary here. Also, make sure x points to a null character at its
+	end to terminate it properly.
+
+	Do not just use the `strcpy` function from the standard library.
 */
 void string_copy(char *x, char *y)
 {
-
+	while (*y != '\0') {
+		*x = *y;
+		x++;
+		y++;
+	}
+	*x = '\0';
 }
 
-/* 
-    Compares the character strings m and n and returns negative,
-    0, or positive if n is lexicographically less than, equal to,
-    or greater than n. To calculate lexicographic difference, find
-    the difference between the first characters in m and n that differ.
-    
-    For example, given matching strings, this function should 
-    return 0. Given strings m = "hello world" and n = "goodbye",
-    this function should return a positive value. Given strings
-    m = "aardvark" and n = "zebra", should return a negative
-    value.
-    
-    Do not just use the `strcmp` function from the standard library.
+/*
+	Compares the character strings m and n and returns negative,
+	0, or positive if n is lexicographically less than, equal to,
+	or greater than n. To calculate lexicographic difference, find
+	the difference between the first characters in m and n that differ.
+
+	For example, given matching strings, this function should
+	return 0. Given strings m = "hello world" and n = "goodbye",
+	this function should return a positive value. Given strings
+	m = "aardvark" and n = "zebra", should return a negative
+	value.
+
+	Do not just use the `strcmp` function from the standard library.
 */
 int string_compare(char *m, char *n)
 {
+	while (*m != '\0' && *n != '\0') {
+		if (*m != *n) {
+			if (*m > *n) {
+				return 1;
+			}
+			else {
+				return -1;
+			}
+		}
+		m++;
+		n++;
+	}
+	if (*m == '\0' && *n == '\0') {
+		return 0;
+	}
+	else if (*m == '\0') {
+		return -1;
+	}
+	else {
+		return 1;
+	}
 
 }
 
 /*
-    Searches the input string `haystack` for the first instance of
-    the string `needle`. This function returns a pointer that points
-    to the first instance of the string `needle` in the input
-    string `haystack`. 
+	Searches the input string `haystack` for the first instance of
+	the string `needle`. This function returns a pointer that points
+	to the first instance of the string `needle` in the input
+	string `haystack`.
 
-    Do not use the `strstr` function from the standard library.
+	Do not use the `strstr` function from the standard library.
 */
 char *find_string(char *haystack, char *needle)
 {
+	char *first = NULL;
 
+	while (*haystack != '\0') {
+		if (*haystack == *needle) {
+			first = *haystack;
+			while (*haystack == *needle) {
+				needle++;
+				haystack++;
+				if (*needle == '\0') {
+					return first;
+				}
+				else if (*haystack == '\0') {
+					return NULL;
+				}
+
+			}
+		}
+		else {
+			haystack++;
+		}
+	}
+	return NULL;
 }
 
 #ifndef TESTING
 int main(void)
 {
-    int x = 10, y = 20;
-    swap(&x, &y);
-    printf("x=%d, y=%d\n", x, y);
+	int x = 10, y = 20;
+	swap(&x, &y);
+	printf("x=%d, y=%d\n", x, y);
 
-    char *hello = "Hello";
-    char *world = "World";
-    char buffer[1024];
+	char *hello = "Hello";
+	char *world = "World";
+	char buffer[1024];
 
-    string_copy(buffer, hello);
+	string_copy(buffer, hello);
 
-    printf("Buffer is %s\n", buffer);
-    printf("Comparison is %d\n", string_compare(hello, world));
+	printf("Buffer is %s\n", buffer);
+	printf("Comparison is %d\n", string_compare(hello, world));
 
-    char *found_char = find_char(hello, 'e');
-    char *found_string = find_string(world, "or");
+	char *found_char = find_char(hello, 'e');
+	char *found_string = find_string(world, "or");
 
-    printf("Found char: %s\n", found_char);
-    printf("Found string: %s\n", found_string);
+	printf("Found char: %s\n", found_char);
+	printf("Found string: %s\n", found_string);
 
-    return 0;
+	return 0;
 }
 #endif
--- strings/strings.c
+++ strings/strings.c
@@ -1,46 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 /*
-    Given a character array s, return the number of characters 
-    held inside it.
-    
-    Do not just use the `strlen` function from the standard libary.
+	Given a character array s, return the number of characters
+	held inside it.
+
+	Do not just use the `strlen` function from the standard libary.
 */
 int string_length(char s[])
-{	
-	return sizeof(s);
+{
+	int len = 0;
+
+	while (s[len] != '\0') {
+		len++;
+	}
+	return len;
 }
 
 /*
-    Write a function that reverses the order of string s and outputs 
-    the reversed string to the input array rv. The rv array will have 
-    enough space for the reversed string. Don't forget to terminate 
-    the reversed string with a null character. Return the rv array.
+	Write a function that reverses the order of string s and outputs
+	the reversed string to the input array rv. The rv array will have
+	enough space for the reversed string. Don't forget to terminate
+	the reversed string with a null character. Return the rv array.
 */
 char *reverse_string(char rv[], char s[])
 {
 	int count = 0;
-	for(int i = string_length(s); i > 0; --i){
-		rv[count] = s[i];
+	int len = string_length(s);
+	for (int i = len - 1; i >= 0; i--) {
+		rv[count++] = s[i];
 	}
-	printf("%s",rv);
-	s = NULL;
+	rv[len] = '\0';
 	return rv;
 }
 
 #ifndef TESTING
 int main(void)
 {
-    char quote1[] = "Don't forget to be awesome";
-    char quote2[] = "a man a plan a canal panama";
+	char quote1[] = "Don't forget to be awesome";
+	char quote2[] = "a man a plan a canal panama";
 
-    char rv[512];
+	char rv[512];
 
-    printf("The string 'Don't forget to be awesome' has %d characters.\n", string_length(quote1));
-    printf("The string 'a man a plan a canal panama' reversed is: '%s'\n", reverse_string(rv, quote2));
+	printf("The string 'Don't forget to be awesome' has %d characters.\n", string_length(quote1));
+	printf("The string 'a man a plan a canal panama' reversed is: '%s'\n", reverse_string(rv, quote2));
 
-    return 0;
+	return 0;
 }
 #endif
-    
+
