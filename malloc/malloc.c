d52fd43408b1b7a0a7d59e997dc70a7a12780b06
dirupt <dirupt@DESKTOP-HEUGRKD.localdomain>
2018-10-02 14:38:33 -0500

    finished malloc

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -13,7 +13,15 @@
 */
 char *string_dup(char *src)
 {
+    int length = string_length(src);
+    char *copy = malloc(sizeof(char) * length);
 
+    for (int i = 0; i < length; i++)
+    {
+        copy[i] = *src;
+        src++;
+    }
+    return copy;
 }
 
 /*
@@ -26,7 +34,14 @@ char *string_dup(char *src)
 */
 void *mem_copy(void *dest, const void *src, int n)
 {
+    char *dest_copy = (char *)dest;
+    char *src_copy = (char *)src;
 
+    for (int i = 0; i < n; i++)
+    {
+        dest_copy[i] = *src_copy;
+        src_copy++;
+    }
 }
 
 /*
@@ -43,7 +58,9 @@ void *mem_copy(void *dest, const void *src, int n)
 */
 void *resize_memory(void *ptr, int old_size, int new_size)
 {
-
+    char *ptr_copy = malloc(new_size);
+    mem_copy(ptr_copy, ptr, new_size);
+    return ptr_copy;
 }
 
 #ifndef TESTING
@@ -57,12 +74,13 @@ int main(void)
     int numbers[] = {100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79};
     int n = sizeof(numbers) / sizeof(numbers[0]);
     int *target = malloc(n * sizeof(int));
-    
+
     mem_copy(target, numbers, n * sizeof(int));
 
     printf("Copied array: ");
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         printf("%d ", target[i]);
     }
 
@@ -72,12 +90,13 @@ int main(void)
     char *path = string_dup("/students/");
     int url_length = string_length(url);
     int path_length = string_length(path);
-    
+
     int new_length = url_length - 1 + path_length;
     char *new_url = resize_memory(url, url_length, new_length);
     char *p = new_url + url_length;
 
-    while (*path != '\0') {
+    while (*path != '\0')
+    {
         *p = *path;
         p++;
         path++;
