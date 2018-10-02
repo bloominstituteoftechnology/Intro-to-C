5dda3ddf8027c4314f8df1ce7df1ef6b467313db
Katie Gorbell <katiegorbell@gmail.com>
2018-10-02 18:16:24 -0500

    strings completed

--- strings/strings.c
+++ strings/strings.c
@@ -9,18 +9,31 @@
 */
 int string_length(char s[])
 {
-
+    int count = 0;
+    while(*s != '\0')
+    {
+        count++;
+        s++;
+    }
+    return count;
 }
 
 /*
-    Write a function that reverses the order of string s and outputs 
+    Write a function thatintro to c reverses the order of string s and outputs 
     the reversed string to the input array rv. The rv array will have 
     enough space for the reversed string. Don't forget to terminate 
     the reversed string with a null character. Return the rv array.
+
+    str length, for loop: assign rv moving backwards from s, rv[len] = '\0'
 */
 char *reverse_string(char rv[], char s[])
-{
-
+{ 
+    int len = strlen(s);
+    for (int i = 0; i < len; i++) {
+        rv[i] = s[len - 1 - i];
+    }
+    rv[len] = '\0';
+    return rv;
 }
 
 #ifndef TESTING
