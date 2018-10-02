9c9052448cd80e777b57158c985bf8ec70ee5526
Simon <Simon_Freelance@hotmail.com>
2018-10-02 15:58:42 -0400

    Solution to quicksort added

--- quicksort/quicksort.c
+++ quicksort/quicksort.c
@@ -15,18 +15,56 @@
     
     Do not just use the `qsort` function from the standard library.
 */
-void quicksort(int *arr, int low, int high)
-{
 
-}
+int partition (int arr[], int low, int high) 
+{ 
+    // The last element in the array is the pivot
+    int pivot = arr[high];
+    int i = (low - 1);  
+    for (int j = low; j <= high- 1; j++) 
+    { 
+        // If current element is smaller than or 
+        // equal to pivot 
+        if (arr[j] <= pivot) 
+        { 
+            i++;
+            swap(&arr[i], &arr[j]); 
+        } 
+    } 
+    swap(&arr[i + 1], &arr[high]); 
+    return (i + 1); 
+} 
+  
+
+void quicksort(int arr[], int low, int high) 
+{ 
+    if (low < high) 
+    { 
+        int pi = partition(arr, low, high); 
+  
+
+        /*
+        We call quicksort again for smaller values or equal values to the pivot
+        and place them on the left side of the pivot.
+
+        and another time for values bigger than the pivot which are placed on the right side
+        of the pivot.
+        */
+        quicksort(arr, low, pi - 1); 
+        quicksort(arr, pi + 1, high); 
+    } 
+} 
 
 #ifndef TESTING
 int main(void)
 {
     int arr1[] = {100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79};
+    // finding the size of the array
     int n = sizeof(arr1) / sizeof(arr1[0]);
+    // 
     quicksort(arr1, 0, n-1);
 
+    // prints out all the elements in order in the array which quicksort has sorted
     for (int i = 0; i < n; i++) {
         printf("%d ", arr1[i]);
     }
