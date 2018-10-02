56ea8e5873fb9c293480c5dbd93a9eba8c0a6216
JJ Ashcraft <jj@jjashcraft.com>
2018-10-02 13:42:48 -0400

    strings complete

--- strings/strings.c
+++ strings/strings.c
@@ -24,19 +24,28 @@ int string_length(char s[])
 */
 char *reverse_string(char rv[], char s[])
 {
+int s_length = string_length(s);
+int counter = s_length - 1;
 
+for (int i=0; i <= s_length ;i++) {
+  rv[i] = s[counter];
+  counter--;
+}
+rv[s_length] = '\0';
+return rv;
 }
 
 #ifndef TESTING
 int main(void)
 {
     char quote1[] = "Don't forget to be awesome";
-    char quote2[] = "a man a plan a canal panama";
+    char quote2[] = "a man a plan";
 
     char rv[512];
 
+    reverse_string(rv, quote2);
     printf("The string 'Don't forget to be awesome' has %d characters.\n", string_length(quote1));
-    printf("The string 'a man a plan a canal panama' reversed is: '%s'\n", reverse_string(rv, quote2));
+    printf("The string 'a man a plan' reversed is: '%s'\n", reverse_string(rv, quote2));
 
     return 0;
 }
