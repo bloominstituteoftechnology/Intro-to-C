aa50706048d42ff8f9bb9f046c3d458032ef00c1
Adrian Adames <adrianadames@gmail.com>
2018-10-06 00:43:58 -0400

    Done with fizzbuzz.

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,15 @@
 */
 int fizzbuzz(int n)
 {
+    int i;
 
+    for (i = 1; i < n; ++i)
+        if (i % 3 == 0 && i % 5 == 0)
+            printf("%d FizzBuzz\n", i);
+        else if (i % 3 == 0)
+            printf("%d Fizz\n", i);
+        else if (i % 5 == 0)
+            printf("%d Buzz\n", i);
 }
 
 #ifndef TESTING
