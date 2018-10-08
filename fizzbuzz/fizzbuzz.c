314f07a6ca71c5d101d0ce7e90dfa793d064534e
Eric B <timberwolf.eric@gmail.com>
2018-10-08 14:38:31 -0500

    Started on Intro-to-C

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,24 @@
 */
 int fizzbuzz(int n)
 {
-
+    for (int i = 0; i <= n; i += 1)
+    {
+        if (i % 3 == 0)
+        {
+            if (i % 5 == 0)
+            {
+                printf('FizzBuzz\n');
+            }
+            else
+            {
+                printf('Fizz\n');
+            }
+        }
+        else if (i % 5 == 0)
+        {
+            printf('Buzz\n');
+        }
+    }
 }
 
 #ifndef TESTING
