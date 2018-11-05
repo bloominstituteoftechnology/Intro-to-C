df5a17b54f5b02526b87e2ef251ee06cf37cb4e5
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-11-05 13:38:26 -0700

    C Compiler set up and working, fizzbuzz complete

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,17 @@
 */
 int fizzbuzz(int n)
 {
-
+int count; 
+for(int i = 0; i <= n; i++){
+    if((i % 3 == 0) && (i % 5 == 0))
+        printf("Fizzbuzz\n");
+    else if(i % 3 == 0)
+        printf("Fizz\n");
+    else if(i % 5 == 0)
+        printf("Buzz\n");
+    else count++;
+}
+return count;
 }
 
 #ifndef TESTING
