f72accc97d39f161aec208ca87b2c325afe329cc
Arthur Bates <aabatesj@gmail.com>
2018-11-05 17:52:18 -0500

    fizzbuzz
    update to fizzbuzz.c

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,19 @@
 */
 int fizzbuzz(int n)
 {
+    int i;
+    for (i = 1; i <= 100; ++i)
+    {
+        if (i % 3 == 0)
+            printf("Fizz");
+        if (i % 5 == 0)
+            printf("Buzz");
+        if ((i % 3 != 0) && (i % 5 != 0))
+            printf("number=%d", i);
+        printf("\n");
+    }
 
+    return 0;
 }
 
 #ifndef TESTING
