8982fd9c60491309e1a6c145d8afe62ccd4296a6
LizBaker <lizbaker.pdx@gmail.com>
2018-11-05 13:47:32 -0800

    starting to play with C

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,19 @@
 */
 int fizzbuzz(int n)
 {
+    for (int i=0; i<n; ++i){
 
+        if (i % 3 == 0 && i % 5 == 0){
+            printf("fizzbuzz\n");
+        }
+        else if (i % 3 == 0){
+            printf("fizz\n");
+        }
+        else if (i % 5 == 0){
+            printf("buzz\n");
+        }
+        return ++i;
+    }
 }
 
 #ifndef TESTING
