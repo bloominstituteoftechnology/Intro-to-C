09609ab3e4a93a0d52412ca4301fc64ee81416e0
MichaelBoegner <cmikeb@gmail.com>
2018-11-05 18:48:24 -0500

    Finished first half of strings

--- strings/strings.c
+++ strings/strings.c
@@ -7,9 +7,14 @@
     
     Do not just use the `strlen` function from the standard libary.
 */
-int string_length(char s[])
-{
+int string_length(char s[]) {
+  int counter = 0;
+  
+  for(int i = 0; s[i] != '\0'; i++){
+    counter = counter + 1;  
+  }
 
+  return counter;
 }
 
 /*
