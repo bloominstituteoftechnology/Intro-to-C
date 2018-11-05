aa6b862f7ce0b2ee310a47b4c901329ce216db49
jordan-massingill <jordan_massingill@hotmail.com>
2018-11-05 17:53:59 -0600

    Working on pointers

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -2,17 +2,21 @@
 
 /*
     Swaps the integer values being pointed at by a and b. Keep in
-    mind when you need to access a pointer's actual value (the 
-    address it's referring to) or the value at the address it's 
+    mind when you need to access a pointer's actual value (the
+    address it's referring to) or the value at the address it's
     pointing at.
 */
 void swap(int* a, int* b)
 {
-
+  int temp = a;
+  char *pntr = &a;
+  char *pntrb = &b;
+  *pntr = b;
+  *pntrb = temp;
 }
 
 /*
-    Searches the input string `str` for the first instance of the 
+    Searches the input string `str` for the first instance of the
     character `c` (an unsigned char). This function returns a pointer
     that points to the first instance of the character `c` in the
     input string `str`.
@@ -21,15 +25,15 @@ void swap(int* a, int* b)
 */
 char *find_char(char *str, int c)
 {
-
+  
 }
 
 /*
     Given an empty (NULL) character pointer x and a character pointer y,
     copies the character contents of y over to x. Again, pointer arithmetic
-    is necessary here. Also, make sure x points to a null character at its 
-    end to terminate it properly. 
-    
+    is necessary here. Also, make sure x points to a null character at its
+    end to terminate it properly.
+
     Do not just use the `strcpy` function from the standard library.
 */
 void string_copy(char *x, char *y)
@@ -37,18 +41,18 @@ void string_copy(char *x, char *y)
 
 }
 
-/* 
+/*
     Compares the character strings m and n and returns negative,
     0, or positive if n is lexicographically less than, equal to,
     or greater than n. To calculate lexicographic difference, find
     the difference between the first characters in m and n that differ.
-    
-    For example, given matching strings, this function should 
+
+    For example, given matching strings, this function should
     return 0. Given strings m = "hello world" and n = "goodbye",
     this function should return a positive value. Given strings
     m = "aardvark" and n = "zebra", should return a negative
     value.
-    
+
     Do not just use the `strcmp` function from the standard library.
 */
 int string_compare(char *m, char *n)
@@ -60,7 +64,7 @@ int string_compare(char *m, char *n)
     Searches the input string `haystack` for the first instance of
     the string `needle`. This function returns a pointer that points
     to the first instance of the string `needle` in the input
-    string `haystack`. 
+    string `haystack`.
 
     Do not use the `strstr` function from the standard library.
 */
