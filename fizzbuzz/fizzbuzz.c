a6dea88688f4d1200db3aad001cd057b2d2f901e
francistse23 <wtse.ucsd@gmail.com>
2018-11-05 11:56:08 -0800

    Franics Tse - Intro to C

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,26 @@
 */
 int fizzbuzz(int n)
 {
-
+    int counter = 0;
+    for (int i = 1; i <= n; i++) {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            printf("FizzBuzz \n");
+        } 
+        else if (i % 3 == 0)
+        {
+            printf("Fizz \n");
+        }
+        else if (i % 5 == 0)
+        { 
+            printf("Buzz \n");
+        }
+        else
+        {
+            counter += 1;
+        }
+    }
+    return counter;
 }
 
 #ifndef TESTING
