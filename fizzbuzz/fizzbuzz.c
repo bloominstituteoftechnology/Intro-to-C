6844c473117d12229ad17234681a9b684922cf4f
meltzerkyle <meltzerkyle@gmail.com>
2018-11-05 16:41:37 -0600

    fizzbuzz

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,31 @@
 */
 int fizzbuzz(int n)
 {
+    int i;
+    int counter = 0;
 
+    for (i = 0; i < n; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            printf("fizzbuzz\n");
+        }
+        else if (i % 3 == 0)
+        {
+            printf("fizz\n");
+        }
+        else if (i % 5 == 0)
+        {
+            printf("buzz\n");
+        }
+        else
+        {
+            counter += 1;
+            printf("counter: %d\n", counter);
+        }
+    }
+
+    return counter;
 }
 
 #ifndef TESTING
