a5ab2a0cde9f21fd84ec618a9f2ab01c37984df9
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-11-05 15:38:30 -0800

    complete malloc

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -13,7 +13,14 @@
 */
 char *string_dup(char *src)
 {
-
+    int len = string_length(src);
+    char *dup = malloc(len+1);
+    for (int i = 0; i < len; i++) {
+        *(dup+i) = *(src+i);
+        dup[i] = src[i];
+    }
+    *(dup + len) = '\0';
+    return dup;
 }
 
 /*
@@ -26,7 +33,12 @@ char *string_dup(char *src)
 */
 void *mem_copy(void *dest, const void *src, int n)
 {
+    char *pdest = (char *) dest;
+    char *psrc = (char *) src;
 
+    for (int i = 0; i < n; i++) {
+        *(pdest+i) = *(psrc+i);
+    }
 }
 
 /*
@@ -43,7 +55,15 @@ void *mem_copy(void *dest, const void *src, int n)
 */
 void *resize_memory(void *ptr, int old_size, int new_size)
 {
-
+    char *new = (char *)malloc(new_size * sizeof(char));
+    char *pointer = ptr;
+    for (int i = 0; i <= old_size; i++) {
+        if (i == new_size) {
+            break;
+        }
+        new[i] = pointer[i];
+    }
+    return new;
 }
 
 #ifndef TESTING
