0f860b4024340d7b910a2aad52b765ee025d5a27
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-11-06 18:38:22 -0500

    some initial hangman work testing.

--- hangman/hangman.c
+++ hangman/hangman.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "lib.h"
 
-char HANG_STATES[7][10 * 9] = 
+char HANG_STATES[7][10 * 9] =
 {
 	"             +         +----     +----     +----     +----     +----     +----     +----  ",
 	"             |         |         |   O     |   O     |   O     |   O     |   O     |   O  ",
@@ -15,6 +16,27 @@ char HANG_STATES[7][10 * 9] =
 
 int main(int argc, char *argv[]) {
   /* Your code here */
-  
+  char *solution = argv[1];
+  int len = string_length(solution);
+  int hang_state = 0;
+  int playing = 1;
+
+  char guess;
+  printf("\nPlease guess a letter: \n");
+  guess = getchar();
+  printf("\nYour Guess: %c\n", guess);
+
+
   return 0;
 }
+
+
+
+
+
+// printf("You wrote: %s\n\n", solution);
+//
+// for(int i = 0; i < 7; i++)
+// {
+  //   printf("%.10s\n", &HANG_STATES[i][hang_state*10]);
+  // }
---
+++ hangman/lib.h
@@ -0,0 +1,11 @@
+#ifndef lib_h
+#define lib_h
+
+#include <string.h>
+
+int string_length(char *s)
+{
+  return strlen(s);
+}
+
+#endif
