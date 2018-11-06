790c441c3217b95464ea71bd3ef717694995e6cc
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-11-06 12:05:48 -0700

    Finished first two pointer exercises

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -6,9 +6,11 @@
     address it's referring to) or the value at the address it's 
     pointing at.
 */
-void swap(int* a, int* b)
+void swap(int *a, int *b)
 {
-
+    int temp = *a;  // assigning the address of a's value to temp
+    *a = *b;        // setting the address of a to the address of b 
+    *b = temp;      // setting the address of b to the value of a's assignment to temp
 }
 
 /*
@@ -21,53 +23,59 @@ void swap(int* a, int* b)
 */
 char *find_char(char *str, int c)
 {
-
+    while( *str){
+        if( *str == c){
+            return str;
+        }
+        str++;
+    }
+    return NULL; 
 }
 
-/*
-    Given an empty (NULL) character pointer x and a character pointer y,
-    copies the character contents of y over to x. Again, pointer arithmetic
-    is necessary here. Also, make sure x points to a null character at its 
-    end to terminate it properly. 
+// /*
+//     Given an empty (NULL) character pointer x and a character pointer y,
+//     copies the character contents of y over to x. Again, pointer arithmetic
+//     is necessary here. Also, make sure x points to a null character at its 
+//     end to terminate it properly. 
     
-    Do not just use the `strcpy` function from the standard library.
-*/
-void string_copy(char *x, char *y)
-{
+//     Do not just use the `strcpy` function from the standard library.
+// */
+// void string_copy(char *x, char *y)
+// {
 
-}
+// }
 
-/* 
-    Compares the character strings m and n and returns negative,
-    0, or positive if n is lexicographically less than, equal to,
-    or greater than n. To calculate lexicographic difference, find
-    the difference between the first characters in m and n that differ.
+// /* 
+//     Compares the character strings m and n and returns negative,
+//     0, or positive if n is lexicographically less than, equal to,
+//     or greater than n. To calculate lexicographic difference, find
+//     the difference between the first characters in m and n that differ.
     
-    For example, given matching strings, this function should 
-    return 0. Given strings m = "hello world" and n = "goodbye",
-    this function should return a positive value. Given strings
-    m = "aardvark" and n = "zebra", should return a negative
-    value.
+//     For example, given matching strings, this function should 
+//     return 0. Given strings m = "hello world" and n = "goodbye",
+//     this function should return a positive value. Given strings
+//     m = "aardvark" and n = "zebra", should return a negative
+//     value.
     
-    Do not just use the `strcmp` function from the standard library.
-*/
-int string_compare(char *m, char *n)
-{
+//     Do not just use the `strcmp` function from the standard library.
+// */
+// int string_compare(char *m, char *n)
+// {
 
-}
+// }
 
-/*
-    Searches the input string `haystack` for the first instance of
-    the string `needle`. This function returns a pointer that points
-    to the first instance of the string `needle` in the input
-    string `haystack`. 
+// /*
+//     Searches the input string `haystack` for the first instance of
+//     the string `needle`. This function returns a pointer that points
+//     to the first instance of the string `needle` in the input
+//     string `haystack`. 
 
-    Do not use the `strstr` function from the standard library.
-*/
-char *find_string(char *haystack, char *needle)
-{
+//     Do not use the `strstr` function from the standard library.
+// */
+// char *find_string(char *haystack, char *needle)
+// {
 
-}
+// }
 
 #ifndef TESTING
 int main(void)
@@ -77,19 +85,19 @@ int main(void)
     printf("x=%d, y=%d\n", x, y);
 
     char *hello = "Hello";
-    char *world = "World";
-    char buffer[1024];
+    // char *world = "World";
+    // char buffer[1024];
 
-    string_copy(buffer, hello);
+    // string_copy(buffer, hello);
 
-    printf("Buffer is %s\n", buffer);
-    printf("Comparison is %d\n", string_compare(hello, world));
+    // printf("Buffer is %s\n", buffer);
+    // printf("Comparison is %d\n", string_compare(hello, world));
 
     char *found_char = find_char(hello, 'e');
-    char *found_string = find_string(world, "or");
+    // char *found_string = find_string(world, "or");
 
     printf("Found char: %s\n", found_char);
-    printf("Found string: %s\n", found_string);
+    // printf("Found string: %s\n", found_string);
 
     return 0;
 }
