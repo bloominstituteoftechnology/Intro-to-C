2c90bfaeeb7f95ef5556c22943701f1f4d3ebb52
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-11-06 13:20:59 -0800

    1st PR

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,27 @@
 */
 int fizzbuzz(int n)
 {
+    for (int i = 1; i <= n; i++ )
+    if(i % 3 == 0 && i % 5 == 0) {
+        printf("FizzBuzz\n");
 
+        return 0;
+    }
+    else if (i % 3 == 0) {
+        printf("Fizz\n");
+
+        return 0;
+    }
+    else if (i % 5 == 0) {
+        printf("Buzz\n");
+
+        return 0;
+    }
+    else {
+        printf("%d", i);
+
+        return 0;
+    }
 }
 
 #ifndef TESTING
