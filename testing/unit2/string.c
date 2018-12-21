1ec50d686eb7fdd33868b5fa7c8d56bf1836b703
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-12-21 09:15:19 -0800

    string

--- testing/unit2/string.c
+++ testing/unit2/string.c
@@ -10,3 +10,19 @@
 //     printf("The new word is %s\n", word);
 //     return 0;
 // }
+
+// Your program should first read the total number of names (an integer) in order to know how many index cards need to be processed altogether. Next,  for each index card, your program should read a patron's first name and last name and then display these names correctly, that is on one line, the last name followed by one space, followed by the first name.  Your program should print the reversed name immediately after reading the patron's names (ie, it should not wait until it has read all of the index cards to begin printing).
+#include <stdio.h>
+int main(void)
+{
+    int num, i;
+    char first[101], last[101];
+    scanf("%d", &num);
+    for (i = 0; i < num; i++)
+    {
+        scanf("%s", &first);
+        scanf("%s", &last);
+        printf("%s %s\n", last, first);
+    }
+    return 0;
+}
