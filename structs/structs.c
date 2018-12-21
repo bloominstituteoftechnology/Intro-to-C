a9b832b0b8613345be6d74e333c65280c27457dd
Adrian Adames <adrianadames@gmail.com>
2018-12-21 00:20:34 -0500

    Almost done with structs exercises. Having issues allocating memory for the 'name' entry in the Person struct.

--- structs/structs.c
+++ structs/structs.c
@@ -22,9 +22,33 @@ typedef struct Person {
     When setting the `name` field, use your previously-built string_dup
     function, which handles the allocation of memory for the new string.
 */
+
+// char *string_dup(char *src)
+// {
+//     char *dup = malloc(strlen(src));
+//     int i;
+//     for (i = 0; i < strlen(src); i++) {
+//         *(dup+i) = *(src+i); 
+//     }
+//     *(dup + strlen(src))='\0';
+//     // free(dup);
+//     return dup;
+// }
 Person *createPerson(char *name, int age, int height, int weight)
 {
-
+    // Person *newPerson;
+    // printf("size of newPerson struct:%d\0", sizeof(newPerson));
+    char nameDup = string_dup(name);
+    Person *newPerson = malloc(sizeof(Person));
+    printf("size of newPerson struct:%d\0", sizeof(Person));
+    
+    // // *newPerson = (struct Person) {*nameDup,age, height,weight};
+    newPerson->name = nameDup;
+    newPerson->age = age;
+    newPerson->height = height;
+    newPerson->weight = weight;
+    // printf("Age: %d\n", newPerson->age);
+    return newPerson;
 }
 
 /*
@@ -33,7 +57,11 @@ Person *createPerson(char *name, int age, int height, int weight)
 */
 void destroyPerson(Person *who)
 {
-
+    // char *name = (*who).name;
+    // printf("name: %c", *name);
+    // free(name);
+    // free((*who).name);
+    free(who);
 }
 
 #ifndef TESTING
