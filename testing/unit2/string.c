5e0653dadc19682fb2ee3079e6825722da1fbb8e
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-12-20 22:45:40 -0800

    strings

---
+++ testing/unit2/string.c
@@ -0,0 +1,12 @@
+// #include <stdio.h>
+// int main(void) {
+//     //! showArray(word, cursors[i])
+//     char word[51]; //arrray of characters (string)
+//     printf("Please enter a word with 50 letters: ");
+//     scanf("%s", word);
+//     printf("The word read is: %s.\n", word);
+//     printf("The characters are: <%c> <%c> <%c> <%c>\n", word[0], word[1], word[2], word[3]);
+//     word[1] = 'a';
+//     printf("The new word is %s\n", word);
+//     return 0;
+// }
