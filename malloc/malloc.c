15570d12014b0dc4ad21a56a8a449f9e3a41b470
Adrian Adames <adrianadames@gmail.com>
2018-12-20 23:01:45 -0500

    Finished working on mem_copy. resize_memory in progress.

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -21,6 +21,7 @@ char *string_dup(char *src)
         // printf("%c\n", *(dup+i));
     }
     *(dup + strlen(src))='\0'; // QQQQQ?: Do I need this here? I think so but would like to make sure. 
+    free(dup);
     return dup;
 }
 
@@ -33,26 +34,24 @@ char *string_dup(char *src)
     Do not use the `memcpy` function from the standard library.
 */
 void *mem_copy(void *dest, const void *src, int n)
-{   
-    // // char *src;
-    // // char *dest;
-    // // n = sizeof(src);
-    // // int i;
-    // // for (i = 0; i < n; i++) {
-    // //     // char*(*(dest+i)) = char*(*(src+i));
-    // //     *(dest+i) = *(src+i);
+{ 
+    // printf("dest size: %i, src size: %i", sizeof(dest), sizeof(src));
+    // printf("dest: %p, src: %p", dest + 1, src+1);
+    
+    // QQQQQQ? casting input pointer to char pointer first. doesn't that change mean change dest from void to char?
 
-    // // }
-    // n = sizeof(src);
-    // char*dest = malloc(n);
-    // int i;
-    // for (i = 0; i < n; i++) {
-    //     char *(dest+i) = *(src+i);
+    // // // ATTEMPT #1     QQQQQQ?: Don't understand why this didn't work
+    // for (int i = 0; i < n; i++) {
+    //     *(&dest + i) = *(&src +i);
     // }
 
-    // printf("%d\n", n);
-    // printf("%p\n", dest);
+    // // ATTEMPT #2
+    char *cdest = dest;
+    const char *csrc = src;
 
+    for (int i = 0; i < n; i++) {
+        *(cdest + i) = *(csrc +i);
+    }    
 }
 
 /*
@@ -69,7 +68,32 @@ void *mem_copy(void *dest, const void *src, int n)
 */
 void *resize_memory(void *ptr, int old_size, int new_size)
 {
+    // ptr: pointer of type void that just malloc'd memory
+    printf("size of input pointer: %d, size of input pointer data type: %d\n", sizeof(ptr),sizeof(ptr[0]));
+    printf("size of old_size: %d, size of new_size: %d\n", sizeof(old_size),sizeof(new_size));
 
+    // need to `re`-malloc ptr and pass in the new_size
+    // if new_size> old_size, data will be intact in newly malloc'd ptr
+    // if new_size< old_size, data will not stay intact
+
+    char *cptr = ptr;
+    if (new_size<old_size) {
+        printf("Error: new_size < old_size, implying that data would be lost if this were to be carried out.\n");
+    }
+    if (new_size>=old_size) {
+        // ptr = (void *) malloc(new_size);
+        cptr = malloc(new_size);
+    }
+    *(cptr+new_size) = "\0"; 
+
+    // for (int i = 0; i < sizeof(ptr);i++) {
+    //     printf("%d\0",(ptr+i));
+    // }
+    printf("%d\0",sizeof(cptr));
+    printf("cptr:%p (hex), %d (reg); ptr[0]:%p (hex), %d (reg)\0", cptr,cptr,ptr, ptr);
+    printf("cptr:%p, *cptr:%p, ptr:%p\0", cptr,cptr,ptr);
+    
+    free(cptr);
 }
 
 #ifndef TESTING
@@ -80,28 +104,28 @@ int main(void)
 
     printf("Duplicated string: %s\n", dup);
 
-    // int numbers[] = {100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79};
-    // int n = sizeof(numbers) / sizeof(numbers[0]);
-    // int *target = malloc(n * sizeof(int));
+    int numbers[] = {100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79};
+    int n = sizeof(numbers) / sizeof(numbers[0]);
+    int *target = malloc(n * sizeof(int));
     
-    // mem_copy(target, numbers, n * sizeof(int));
+    mem_copy(target, numbers, n * sizeof(int));
 
-    // printf("Copied array: ");
+    printf("Copied array: ");
 
-    // for (int i = 0; i < n; i++) {
-    //     printf("%d ", target[i]);
-    // }
+    for (int i = 0; i < n; i++) {
+        printf("%d ", target[i]);
+    }
 
-    // printf("\n");
+    printf("\n");
 
-    // char *url = string_dup("http://lambdaschool.com");
-    // char *path = string_dup("/students/");
-    // int url_length = string_length(url);
-    // int path_length = string_length(path);
+    char *url = string_dup("http://lambdaschool.com");
+    char *path = string_dup("/students/");
+    int url_length = string_length(url);
+    int path_length = string_length(path);
     
-    // int new_length = url_length - 1 + path_length;
-    // char *new_url = resize_memory(url, url_length, new_length);
-    // char *p = new_url + url_length;
+    int new_length = url_length - 1 + path_length;
+    char *new_url = resize_memory(url, url_length, new_length);
+    char *p = new_url + url_length;
 
     // while (*path != '\0') {
     //     *p = *path;
