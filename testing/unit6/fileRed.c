ca7d0ffdf47b64aa3bbcb2da2feb1cf8263b6cff
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-12-29 18:47:09 -0800

    file redirection to simulate an input

---
+++ testing/unit6/fileRed.c
@@ -0,0 +1,13 @@
+// nano file.txt   // create+write
+// cat file.txt    // read a file
+// echo "hello"    // output "hello"
+// echo "hello" > file2.txt    // write in a new file (or rewrite)
+// cat file2.txt > file3.txt   // copied from file2 to file3
+// cat > file4.txt     // write smth in a new file (file4) (ctr+D)
+// cat /etc/services | more // space bar to continue
+// cat /etc/services | less // space bar to continue, j, k, arrow keys
+
+// car answers | ./program      // injecting input into a ./program (scanf)
+// or
+// ./program < answers          // ./program (scanf) gets answers (from right to left)
+// Don’t use scanf, use fgets getline or readline
