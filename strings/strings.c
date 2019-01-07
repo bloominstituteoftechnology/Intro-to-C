c3c7982263b61a17390010a9d41eb3aac81b6f36
Timothy Hoang <timh1203@gmail.com>
2019-01-07 16:53:02 -0600

    Add strings

--- strings/strings.c
+++ strings/strings.c
@@ -9,7 +9,15 @@
 */
 int string_length(char s[])
 {
+  int len = 0; // initiate a counter to track letters of a strings
 
+  for (int i = 0; s[i] != '\0'; i++) // loop until hits null character (terminating condition)
+  {
+    len++; // increments length counter for every letter in string
+  }
+
+  // printf("length of string: %i \n", length); // prints length of the string
+  return len;
 }
 
 /*
@@ -20,21 +28,32 @@ int string_length(char s[])
 */
 char *reverse_string(char rv[], char s[])
 {
+  int pos = 0;                  // set starting position of rv string
+  int s_len = string_length(s); // length of the string
 
+  for (int i = s_len - 1; i >= 0; i--) // length of the string minus 1 because of zero-index
+  {
+    rv[pos] = s[i]; // for each position in reversed string (rv), set the letter from the string (s)
+    pos++;          // move to the next position in the reversed string
+  }
+
+  rv[pos] = '\0'; // set the last position to null character to terminate the string
+
+  // printf("Reversed string: %s --> %s \n", s, rv); // prints the old string --> reversed string
+  return rv;
 }
 
 #ifndef TESTING
 int main(void)
 {
-    char quote1[] = "Don't forget to be awesome";
-    char quote2[] = "a man a plan a canal panama";
+  char quote1[] = "Don't forget to be awesome";
+  char quote2[] = "a man a plan a canal panama";
 
-    char rv[512];
+  char rv[512];
 
-    printf("The string 'Don't forget to be awesome' has %d characters.\n", string_length(quote1));
-    printf("The string 'a man a plan a canal panama' reversed is: '%s'\n", reverse_string(rv, quote2));
+  printf("The string 'Don't forget to be awesome' has %d characters.\n", string_length(quote1));
+  printf("The string 'a man a plan a canal panama' reversed is: '%s'\n", reverse_string(rv, quote2));
 
-    return 0;
+  return 0;
 }
 #endif
-    
--- strings/tests/strings_tests.c
+++ strings/tests/strings_tests.c
@@ -4,40 +4,40 @@
 
 char *test_string_length()
 {
-    char quote[] = "Don't forget to be awesome";
-    char another_quote[] = "...My spirit is a roaring sea";
+  char quote[] = "Don't forget to be awesome";
+  char another_quote[] = "...My spirit is a roaring sea";
 
-    mu_assert(string_length("") == 0, "Your string_length function did not return the expected length.");
-    mu_assert(string_length(quote) == 26, "Your string_length function did not return the expected length.");
-    mu_assert(string_length(another_quote) == 29, "Your string_length function did not return the expected length.");
+  mu_assert(string_length("") == 0, "Your string_length function did not return the expected length.");
+  mu_assert(string_length(quote) == 26, "Your string_length function did not return the expected length.");
+  mu_assert(string_length(another_quote) == 29, "Your string_length function did not return the expected length.");
 
-    return NULL;
+  return NULL;
 }
 
 char *test_reverse_string()
 {
-    char rv1[512];
-    char rv2[512];
-    char str1[] = "hello world";
-    char str2[] = "a man a plan a canal panama";
-    
-    mu_assert(check_strings(reverse_string(rv1, str1), "dlrow olleh") == 0, "Your reverse_string function did not correctly reverse a string.");
-    mu_assert(string_length(rv1) == 11, "Your reversed string does not have the same length as the original string.");
+  char rv1[512];
+  char rv2[512];
+  char str1[] = "hello world";
+  char str2[] = "a man a plan a canal panama";
 
-    mu_assert(check_strings(reverse_string(rv2, str2), "amanap lanac a nalp a nam a") == 0, "Your reverse_string function did not correctly reverse a string.");
-    mu_assert(string_length(rv2) == 27, "Your reversed string does not have the same length as the original string.");
+  mu_assert(check_strings(reverse_string(rv1, str1), "dlrow olleh") == 0, "Your 'hello world' reverse_string function did not correctly reverse a string.");
+  mu_assert(string_length(rv1) == 11, "Your 'hello world' reversed string does not have the same length as the original string.");
 
-    return NULL;
+  mu_assert(check_strings(reverse_string(rv2, str2), "amanap lanac a nalp a nam a") == 0, "Your 'a man a plan a canal panama' reverse_string function did not correctly reverse a string.");
+  mu_assert(string_length(rv2) == 27, "Your 'a man a plan a canal panama' reversed string does not have the same length as the original string.");
+
+  return NULL;
 }
 
 char *all_tests()
 {
-    mu_suite_start();
+  mu_suite_start();
 
-    mu_run_test(test_string_length);
-    mu_run_test(test_reverse_string);
+  mu_run_test(test_string_length);
+  mu_run_test(test_reverse_string);
 
-    return NULL;
+  return NULL;
 }
 
 RUN_TESTS(all_tests);
