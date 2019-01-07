099b8dffd4acd6a57fb9e8712af75e2d2d603a14
mparedes003 <michelle.g.paredes@gmail.com>
2019-01-07 15:24:58 -0800

    Edited FizBuzz to Fizzbuzz.

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -14,7 +14,7 @@ int fizzbuzz(int n){
 
     for (int i = 0; i <= n; i++){
         if (i % 3 == 0 && i % 5 == 0){
-            printf("FizBuzz\n");
+            printf("FizzBuzz\n");
         } else if (i % 3 == 0){
             printf("Fizz\n");
         } else if (i % 5 == 0){
