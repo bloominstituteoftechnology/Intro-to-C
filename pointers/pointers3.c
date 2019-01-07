fe01df51c9fd0310ea98af016d628ccafdb455a3
Csillag61 <csillagok@gmail.com>
2019-01-07 17:31:15 -0500

    p address and bytes

--- pointers/pointers3.c
+++ pointers/pointers3.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 int main(void)
 {
@@ -10,11 +11,29 @@ int number = 0;
 int *pnumber = NULL;
 
 number = 10;
+
+printf("pnumber's address: %p\n", &number);
+printf("pnumber's value: %d\n\n", number);
+
 pnumber = &number;
+
+printf("pnumber's address: %p\n", (void*) &pnumber);
+printf("pnumber's size: %ld bytes\n", sizeof(pnumber));
 printf("pnumber's value: %p\n", pnumber);
+printf("value pointed to: %d\n", *pnumber);
 
 return 0;
 
 }
 
 //pointers occupy 8 bytes and the adresses have 16 hexadecimal digits
+//a memory address on my mashine is 64 bits
+
+//this exersize prints:
+//pnumber's address: 0x7fffd6728fbc
+//pnumber's value: 10
+
+//pnumber's address: 0x7fffd6728fc0
+//pnumber's size: 8 bytes
+//pnumber's value: 0x7fffd6728fbc
+//value pointed to: 10
