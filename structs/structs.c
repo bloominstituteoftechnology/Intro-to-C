1b431b586ec3649dbdf9c4a7add8c2752411fe70
Nicocchi <nicocchilover@gmail.com>
2019-01-07 16:40:52 -0500

    structs done

--- structs/structs.c
+++ structs/structs.c
@@ -2,26 +2,36 @@
 #include <stdlib.h>
 #include "lib.h"
 
-/* 
+/*
     Define the Person struct by specifying the fields that make up the
-    Person type. Don't forget to specify the type of each field. A 
+    Person type. Don't forget to specify the type of each field. A
     Person should have the fields `name`, `age`, `height`, and `weight`.
 */
 typedef struct Person {
+  char *name;
+  int age;
+  int height;
+  int weight;
 
 } Person;
 
 /*
     Creates an instance of the Person struct that receives all the relevant
     pieces of data associated with a Person instance.
-    Allocates the appropriate amount of memory to hold an instance of the 
+    Allocates the appropriate amount of memory to hold an instance of the
     Person struct, and then sets the struct's fields with the input data.
     When setting the `name` field, use your previously-built string_dup
     function, which handles the allocation of memory for the new string.
 */
 Person *createPerson(char *name, int age, int height, int weight)
 {
+  Person *p = malloc(sizeof(Person));
 
+  p->name = string_dup(name);
+  p->age = age;
+  p->height = height;
+  p->weight = weight;
+  return p;
 }
 
 /*
@@ -30,7 +40,8 @@ Person *createPerson(char *name, int age, int height, int weight)
 */
 void destroyPerson(Person *who)
 {
-
+  free(who->name);
+  free(who);
 }
 
 #ifndef TESTING
