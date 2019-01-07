b653ff0ab0073050b1c26276ecd65d21e5f8139a
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2019-01-07 14:39:38 -0800

    strings passed

--- strings/strings.c
+++ strings/strings.c
@@ -2,25 +2,39 @@
 #include <stdlib.h>
 
 /*
-    Given a character array s, return the number of characters 
+    Given a character array s, return the number of characters
     held inside it.
-    
+
     Do not just use the `strlen` function from the standard libary.
 */
 int string_length(char s[])
 {
-
+  int length = 0;
+  int addressInArray = 0;
+  char *pointer = &s[addressInArray];
+  while (*pointer) {
+    length += 1;
+    addressInArray += 1;
+    pointer = &s[addressInArray];
+  }
+  return length;
 }
 
 /*
-    Write a function that reverses the order of string s and outputs 
-    the reversed string to the input array rv. The rv array will have 
-    enough space for the reversed string. Don't forget to terminate 
+    Write a function that reverses the order of string s and outputs
+    the reversed string to the input array rv. The rv array will have
+    enough space for the reversed string. Don't forget to terminate
     the reversed string with a null character. Return the rv array.
 */
 char *reverse_string(char rv[], char s[])
 {
+  int s_length = string_length(s);
 
+  for (int i = 1; i <= s_length; i ++) {
+    rv[s_length] = 0;
+    rv[s_length-i] = s[i-1];
+  }
+  return rv;
 }
 
 #ifndef TESTING
@@ -37,4 +51,3 @@ int main(void)
     return 0;
 }
 #endif
-    
