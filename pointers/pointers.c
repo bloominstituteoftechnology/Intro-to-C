3eeea161df0cc452e71b8c0a72f9b09a18213e2f
Nicocchi <nicocchilover@gmail.com>
2019-01-07 15:48:23 -0500

    pointers done

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
 /*
     Swaps the integer values being pointed at by a and b. Keep in
-    mind when you need to access a pointer's actual value (the 
-    address it's referring to) or the value at the address it's 
+    mind when you need to access a pointer's actual value (the
+    address it's referring to) or the value at the address it's
     pointing at.
 */
 void swap(int* a, int* b)
 {
+  // Set a temporary int to store a's pointer
+  int temp = *a;
 
+  // Set a to b
+  *a = *b;
+
+  // Set be back to a with the temp var
+  *b = temp;
 }
 
 /*
-    Searches the input string `str` for the first instance of the 
+    Searches the input string `str` for the first instance of the
     character `c` (an unsigned char). This function returns a pointer
     that points to the first instance of the character `c` in the
     input string `str`.
@@ -21,52 +29,97 @@ void swap(int* a, int* b)
 */
 char *find_char(char *str, int c)
 {
+  // Loop str while it doens't hit '\0'
+  while (*str)
+  {
+    // Check if the str = c and return the string
+    if (*str == c) return str;
 
+    // Else, continue
+    str++;
+  }
+
+  // If nothing, return null
+  return NULL;
 }
 
 /*
     Given an empty (NULL) character pointer x and a character pointer y,
     copies the character contents of y over to x. Again, pointer arithmetic
-    is necessary here. Also, make sure x points to a null character at its 
-    end to terminate it properly. 
-    
+    is necessary here. Also, make sure x points to a null character at its
+    end to terminate it properly.
+
     Do not just use the `strcpy` function from the standard library.
 */
 void string_copy(char *x, char *y)
 {
+  // Loop while the string exists
+  while (*y)
+  {
+    // Copy each char from y to x then increase them
+    *x = *y;
+    x++;
+    y++;
+  }
 
+  *x = *y;
 }
 
-/* 
+/*
     Compares the character strings m and n and returns negative,
     0, or positive if n is lexicographically less than, equal to,
     or greater than n. To calculate lexicographic difference, find
     the difference between the first characters in m and n that differ.
-    
-    For example, given matching strings, this function should 
+
+    For example, given matching strings, this function should
     return 0. Given strings m = "hello world" and n = "goodbye",
     this function should return a positive value. Given strings
     m = "aardvark" and n = "zebra", should return a negative
     value.
-    
+
     Do not just use the `strcmp` function from the standard library.
 */
 int string_compare(char *m, char *n)
 {
+  // Loop while m doesn't end
+  for (int i = 0; m[i] != '\0'; i++) {
+    if (m[i] > n[i]) {
+      return 1;
+    }
+    else if (m[i] < n[i]) {
+      return -1;
+    }
+  }
 
+  if (strlen(n) > strlen(m)) {
+    return -1;
+  }
+
+  return 0;
 }
 
 /*
     Searches the input string `haystack` for the first instance of
     the string `needle`. This function returns a pointer that points
     to the first instance of the string `needle` in the input
-    string `haystack`. 
+    string `haystack`.
 
     Do not use the `strstr` function from the standard library.
 */
 char *find_string(char *haystack, char *needle)
 {
+  // Find the char
+  char *init = find_char(haystack, needle[0]);
+  for (int i = 0; needle[i] != '\0'; i++)
+  {
+    // If doesn't find the string, return null
+    if (init[i] != needle[i])
+    {
+      return NULL;
+    }
+  }
 
+  return init;
 }
 
 #ifndef TESTING
