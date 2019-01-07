fd0f34ee526c1098d4115e4e451883d934c41788
cubsfan46350 <cubsfan.46350@msn.com>
2019-01-07 15:23:49 -0600

    initial commit

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,22 @@
 */
 int fizzbuzz(int n)
 {
-
+    for(int i = 0; i <= n; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            printf("FizzBuzz/n");
+        }
+        else
+            if (i % 5 == 0) {
+                printf("Buzz/n");
+            }
+        else
+            if (i % 3 == 0) {
+                printf("Fizz/n");
+            }
+        else{
+            printf("%d/n", i);
+        }
+    }
 }
 
 #ifndef TESTING
