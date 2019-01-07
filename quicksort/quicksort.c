199944f13c21d3812c24f8987baabec55f16373c
Nicocchi <nicocchilover@gmail.com>
2019-01-07 17:08:04 -0500

    quicksort done

--- quicksort/quicksort.c
+++ quicksort/quicksort.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "lib.h" 
+#include "lib.h"
 
 /*
     Implement the Quicksort algorithm. You'll likely want to re-use the
@@ -7,17 +7,51 @@
     already being included for you in this file).
 
     The `low` and `high` parameters indicate the lowest and highest indices
-    of the array that is getting passed in. This is necessary because the 
+    of the array that is getting passed in. This is necessary because the
     function is being passed a pointer to the array, not the contents of the
-    array. We can't easily figure out the length of the array through the 
+    array. We can't easily figure out the length of the array through the
     pointer, so the function receives the highest and lowest indices as
     parameters to circumvent this issue.
-    
+
     Do not just use the `qsort` function from the standard library.
 */
 void quicksort(int *arr, int low, int high)
 {
+  if (low < high)
+  {
+    // Parition the index
+    int pi = partition(arr, low, high);
 
+    // Then separately sort elements before partition and after
+    quicksort(arr, low, pi - 1);
+    quicksort(arr, pi + 1, high);
+  }
+}
+
+/* Takes the last element as pivot, places
+   the pivot element at its correct position in sorted
+   array and places all smaller to left and greater to right
+*/
+int partition (int arr[], int low, int high)
+{
+  // Pivot
+  int pivot = arr[high];
+  // Index of smaller element
+  int i = (low -1);
+
+  for (int j = low; j <= high - 1; j++)
+  {
+    // If current element is smaller than or equal to, increment
+    // index of smaller element
+    if (arr[j] <= pivot)
+    {
+      i++;
+      swap(&arr[i], &arr[j]);
+    }
+  }
+
+  swap(&arr[i + 1], &arr[high]);
+  return (i + 1);
 }
 
 #ifndef TESTING
