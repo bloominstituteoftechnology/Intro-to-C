c58cdb81952f8471a08425abd1206214c4c4c829
Sasha <taylork85@gmail.com>
2019-01-08 17:43:39 -0600

    Fizzbuzz complete

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -9,7 +9,16 @@
     Don't forget to include newlines '\n' in your printf statements!
 */
 int fizzbuzz(int n){ 
-    if n % 15
+    for(int i=1; i<=n; i++){
+    if(i%15 == 0) 
+        printf("%d FizzBuzz\n", i); 
+    else if(i%3 == 0)
+        printf("%d Fizz\n", i);
+    else if(i%5 == 0)
+        printf("%d Buzz\n", i);
+    else
+        printf("%d\n", i);
+    }
 }
 
 #ifndef TESTING
