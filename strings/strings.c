93cad72fd25c01f13f4eceb9c6e2315082fd03ac
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-01-08 12:30:27 -0500

    string

--- strings/strings.c
+++ strings/strings.c
@@ -1,26 +1,41 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
 /*
-    Given a character array s, return the number of characters 
+    Given a character array s, return the number of characters
     held inside it.
-    
+
     Do not just use the `strlen` function from the standard libary.
 */
 int string_length(char s[])
 {
-
+    int count = 0;
+    for(int i =0; s[i] != '\0'; i++){
+        printf("%c \n", s[i]);
+        count = count + 1;
+    }
+    return count;
 }
 
 /*
-    Write a function that reverses the order of string s and outputs 
-    the reversed string to the input array rv. The rv array will have 
-    enough space for the reversed string. Don't forget to terminate 
+    Write a function that reverses the order of string s and outputs
+    the reversed string to the input array rv. The rv array will have
+    enough space for the reversed string. Don't forget to terminate
     the reversed string with a null character. Return the rv array.
 */
 char *reverse_string(char rv[], char s[])
 {
+int length = strlen(s);
+int ind = length-1;
 
+    for(int  i =0; i<length; i++){
+    rv[i] = s[ind];
+    ind--;
+    }
+rv[length] = '\0';
+printf("%s\n", rv);
+return rv;
 }
 
 #ifndef TESTING
@@ -37,4 +52,3 @@ int main(void)
     return 0;
 }
 #endif
-    
