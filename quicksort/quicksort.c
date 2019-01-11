253e71b2619f79028e781009bba799bdd46446b0
Timothy Hoang <timh1203@gmail.com>
2019-01-10 22:33:04 -0600

    Add quicksort

--- quicksort/quicksort.c
+++ quicksort/quicksort.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "lib.h" 
+#include "lib.h" // swap function
 
 /*
     Implement the Quicksort algorithm. You'll likely want to re-use the
@@ -15,9 +15,30 @@
     
     Do not just use the `qsort` function from the standard library.
 */
-void quicksort(int *arr, int low, int high)
+int partition(int *arr, int low, int high)
 {
+  int pivot = low;
 
+  for (int i=low+1; i < high+1; i++)
+  {
+    if (arr[low] >= arr[i])
+    {
+      pivot++;
+      swap(&arr[i], &arr[pivot]);
+    }
+  }
+  swap(&arr[pivot], &arr[low]);
+  return pivot;
+}
+
+void quicksort(int *arr, int low, int high) // O(nlogn)
+{
+  if (low < high)
+  {
+    int pivot = partition(arr, low, high); // finds the pivot
+    quicksort(arr, low, pivot-1); // checks the left half
+    quicksort(arr, pivot+1, high); // checks the right half
+  }
 }
 
 #ifndef TESTING
