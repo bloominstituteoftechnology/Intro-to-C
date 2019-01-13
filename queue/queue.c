7f94e5f8d924e18986a15e35085660ac590b4cce
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2019-01-13 14:01:32 -0800

    structs and queue complete

--- queue/queue.c
+++ queue/queue.c
@@ -3,9 +3,9 @@
 #include "lib.h"
 
 typedef struct Queue {
-    unsigned int length;
-    unsigned int capacity;
-    int *storage;
+    unsigned int length; // current length
+    unsigned int capacity; // max length
+    int *storage; // is this an array of ints from 0 to length?
 } Queue;
 
 /*
@@ -15,7 +15,13 @@ typedef struct Queue {
 */
 Queue *createQueue(unsigned capacity)
 {
+    Queue *q = malloc(sizeof(Queue));
 
+    q->capacity = capacity;
+    q->length = 0;
+    int *storage = malloc(capacity * sizeof(int));
+    q->storage = storage;
+    return q;
 }
 
 /*
@@ -25,7 +31,12 @@ Queue *createQueue(unsigned capacity)
 */
 void enqueue(Queue *q, int item)
 {
+    if (q->length >= q->capacity) {
+        q->capacity++;
+    }
 
+    q->storage[q->length] = item;
+    q->length++;
 }
 
 /*
@@ -34,7 +45,26 @@ void enqueue(Queue *q, int item)
 */
 int dequeue(Queue *q)
 {
+    // If the lengh is equal to 0, nothing to deque, return -1
+    if (q->length == 0){
+        return -1;
+    }
 
+    // Store the removed item
+    int removed = q->storage[0];
+    // Get the length
+    int len = q->length;
+
+    // Remove the item
+    for (int i = 0; i < len; i++){
+        q->storage[i] = q->storage[i + 1];
+    }
+
+    // Resize length
+    q->length--;
+
+    // Return the removed item
+    return removed;
 }
 
 /*
@@ -43,7 +73,8 @@ int dequeue(Queue *q)
 */
 void destroyQueue(Queue *q)
 {
-
+    free(q->storage);
+    free(q);
 }
 
 
--- structs/structs.c
+++ structs/structs.c
@@ -8,7 +8,10 @@
     Person should have the fields `name`, `age`, `height`, and `weight`.
 */
 typedef struct Person {
-
+    char *name;
+    int age;
+    int height;
+    int weight;
 } Person;
 
 /*
@@ -21,7 +24,12 @@ typedef struct Person {
 */
 Person *createPerson(char *name, int age, int height, int weight)
 {
-
+    Person *p = malloc(sizeof(Person));
+    p->name = string_dup(name);
+    p->age = age;
+    p->height = height;
+    p->weight = weight;
+    return p;
 }
 
 /*
@@ -30,7 +38,8 @@ Person *createPerson(char *name, int age, int height, int weight)
 */
 void destroyPerson(Person *who)
 {
-
+    free(who->name);
+    free(who);
 }
 
 #ifndef TESTING
