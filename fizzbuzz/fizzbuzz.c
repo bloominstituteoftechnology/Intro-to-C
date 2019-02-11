d27c1362f773eb09c904c05da0aeb52ef7b9ddb6
Jeanette <jeanfern555@gmail.com>
2019-02-11 17:30:49 -0600

    Completed Fizzbuzz - I am currently not able to test it using 'make tests' because there are issues with my compiler, tested it on repl.it and it seem to run correctly.

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,9 +10,28 @@
 */
 int fizzbuzz(int n)
 {
-
-
-}
+    int counter = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (i % 3 == 0)
+        {
+            printf("Fizz\n");    
+        }
+        else if (i % 5 == 0)
+        {
+            printf("Buzz\n");
+        }
+        else if (i % 3 == 0 && i % 5 == 0)
+        {
+            printf("FizzBuzz\n");
+        }
+        else
+        {
+            counter++;
+        };    
+    };
+    return counter;
+};
 
 #ifndef TESTING
 int main(void)
