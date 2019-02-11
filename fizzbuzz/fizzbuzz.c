5980d5325573b84a894a9b84c61072194a3d096c
Eva <evalee904@gmail.com>
2019-02-11 16:45:40 -0600

    testing fizzbuzz

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -16,15 +16,15 @@ int fizzbuzz(int n)
     {
         if (i % 3 == 0 && i % 5 == 0)
         {
-            printf('FizzBuzz\n');
+            printf("FizzBuzz\n");
         }
         else if (i % 3 == 0)
         {
-            printf('Fizz\n');
+            printf("Fizz\n");
         }
         else if (i % 5 == 0)
         {
-            printf('Buzz\n');
+            printf("Buzz\n");
         }
         else
         {
