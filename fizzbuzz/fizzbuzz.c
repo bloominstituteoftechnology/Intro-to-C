8db74c1598ff88967c1007d27655cff3889ae30b
Vuk Radosavljevic <vukrado1@gmail.com>
2019-02-11 17:03:30 -0600

    Completed fizzbuzz

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -12,7 +12,13 @@ int fizzbuzz(int n)
 {
     
     for (int i = 0; i < n; i++) {
-        printf("%d\n", i);
+        if (i % 3 == 0 && i % 5 == 0) {
+            printf("FizzBuzz\n");
+        } else if (i % 5 == 0) {
+            printf("Buzz\n");
+        } else if (i % 3 == 0) {
+            printf("Fizz\n");
+        }
     }
     return 0;
 }
