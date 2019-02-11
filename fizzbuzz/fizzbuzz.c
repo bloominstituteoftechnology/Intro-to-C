8111aa725284ac96e7eb227108ceadf47f30756d
ezra davis davis <ezra.rr.davis@gmail.com>
2019-02-11 15:46:59 -0800

    fizzbuzz finished

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,22 @@
 */
 int fizzbuzz(int n)
 {
-
+  for(int i = 0; i <= n; i++){
+    if (i % 3 == 0 && i % 5 != 0) {
+      printf("Fizz\n");
+    } if (i % 5 == 0 && i % 3 != 0) {
+      printf("Buzz\n");
+    } if (i % 5 == 0 && i % 3 == 0) {
+      printf("FizzBuzz\n");
+    }
+    
+    
+    
+  }
+  {
+    /* code */
+  }
+  
 }
 
 #ifndef TESTING
