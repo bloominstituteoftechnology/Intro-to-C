a0de2b8f371ad442bdbb252c150df9c0c42a0662
Steven O'Neill <oneillsteven957@gmail.com>
2019-02-11 18:15:58 -0600

    started pointers

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -8,7 +8,9 @@
 */
 void swap(int* a, int* b)
 {
-
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 /*
@@ -21,7 +23,13 @@ void swap(int* a, int* b)
 */
 char *find_char(char *str, int c)
 {
-
+    int i = 0;
+    while(str[i]) {
+        if (str[i] == c) {
+            return &str[i];
+        }
+        i ++;
+    }
 }
 
 /*
