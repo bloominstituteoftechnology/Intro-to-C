21766e25456338b5e4d7607d2ee59a35fa271a32
Chiboba <briangfelix@gmail.com>
2019-02-12 14:35:34 -0800

    Pointers

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
-
+#include <string.h>
 /*
     Swaps the integer values being pointed at by a and b. Keep in
     mind when you need to access a pointer's actual value (the 
     address it's referring to) or the value at the address it's 
     pointing at.
 */
+
+
+
 void swap(int* a, int* b)
 {
-
+    int hold = *b;
+    *b = *a;
+    *a = hold;
 }
 
 /*
@@ -19,9 +24,21 @@ void swap(int* a, int* b)
 
     Do not use the `strchr` function from the standard library.
 */
+
 char *find_char(char *str, int c)
 {
-
+    printf("%i", c);
+        printf("%d", *str);
+    
+    while(*str != '\0') {
+        
+        if(*str == c) {
+            char *temp = str;
+            return temp;
+        }
+        str++;
+    }
+    return NULL;
 }
 
 /*
@@ -34,7 +51,18 @@ char *find_char(char *str, int c)
 */
 void string_copy(char *x, char *y)
 {
-
+    int length = strlen(y);
+    printf("%c\n", *y);
+    printf("%c\n", *x);
+    for(int i = 0; i < length; i ++) {
+        *x = *y;
+        printf("%c\n", *x);
+        printf("%c\n", *y);
+        x++;
+        y++;   
+    }
+    *x = '\0';
+    printf("%s", y);  
 }
 
 /* 
@@ -51,8 +79,17 @@ void string_copy(char *x, char *y)
     
     Do not just use the `strcmp` function from the standard library.
 */
+
 int string_compare(char *m, char *n)
 {
+    while(*m == *n) {
+        if(*m == '\0') {
+            return 0;
+        }
+        m++;
+        n++;
+    }
+    return *m - *n;
 
 }
 
@@ -64,33 +101,53 @@ int string_compare(char *m, char *n)
 
     Do not use the `strstr` function from the standard library.
 */
-char *find_string(char *haystack, char *needle)
-{
 
+char *find_string(char *haystack, char *needle)
+{   
+    
+    char *temp = needle;
+
+    while(*haystack != '\0') {
+        if(*haystack == *needle) {
+            
+            char *start = haystack;
+            while(*needle == *haystack && *needle != '\0') {
+                needle++;
+                haystack++;
+            }
+            if(*needle == '\0') {
+                return start;
+            }
+            needle = temp;
+        }
+        haystack++;
+    }
+    return NULL;
 }
 
 #ifndef TESTING
 int main(void)
 {
+    
     int x = 10, y = 20;
     swap(&x, &y);
     printf("x=%d, y=%d\n", x, y);
-
+    
     char *hello = "Hello";
     char *world = "World";
     char buffer[1024];
-
-    string_copy(buffer, hello);
-
+    
+    
     printf("Buffer is %s\n", buffer);
     printf("Comparison is %d\n", string_compare(hello, world));
-
+    
     char *found_char = find_char(hello, 'e');
+
     char *found_string = find_string(world, "or");
 
     printf("Found char: %s\n", found_char);
     printf("Found string: %s\n", found_string);
-
+    
     return 0;
 }
 #endif
