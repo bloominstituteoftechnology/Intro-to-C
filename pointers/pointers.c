7591674ed4ca890973ad3218c5d0bb760e79d57f
Conner Alegre <cga0@pm.me>
2019-02-11 19:41:12 -0700

    Complete pointers

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /*
     Swaps the integer values being pointed at by a and b. Keep in
@@ -21,10 +22,20 @@ void swap(int* a, int* b)
 
     Do not use the `strchr` function from the standard library.
 */
-// char *find_char(char *str, int c)
-// {
+char *find_char(char *str, int c)
+{
+    int len = strlen(str);
+    for (int i = 0; i < len; i++)
+    {
+        int num_letter = str[i];
 
-// }
+        if (num_letter == c)
+        {
+            return &str[i];
+        }
+    }
+    return NULL;
+}
 
 /*
     Given an empty (NULL) character pointer x and a character pointer y,
@@ -45,37 +56,90 @@ void string_copy(char *x, char *y)
     x[i] = '\0';
 }
 
-// /* 
-//     Compares the character strings m and n and returns negative,
-//     0, or positive if n is lexicographically less than, equal to,
-//     or greater than n. To calculate lexicographic difference, find
-//     the difference between the first characters in m and n that differ.
+/* 
+    Compares the character strings m and n and returns negative,
+    0, or positive if n is lexicographically less than, equal to,
+    or greater than n. To calculate lexicographic difference, find
+    the difference between the first characters in m and n that differ.
     
-//     For example, given matching strings, this function should 
-//     return 0. Given strings m = "hello world" and n = "goodbye",
-//     this function should return a positive value. Given strings
-//     m = "aardvark" and n = "zebra", should return a negative
-//     value.
+    For example, given matching strings, this function should 
+    return 0. Given strings m = "hello world" and n = "goodbye",
+    this function should return a positive value. Given strings
+    m = "aardvark" and n = "zebra", should return a negative
+    value.
     
-//     Do not just use the `strcmp` function from the standard library.
-// */
-// int string_compare(char *m, char *n)
-// {
+    Do not just use the `strcmp` function from the standard library.
+*/
+int string_compare(char *m, char *n)
+{
+    int m_len = strlen(m);
+    int n_len = strlen(n);
+    int highest = 0;
 
-// }
+    if (m_len > n_len)
+    {
+        highest = m_len;
+    }
+    else
+    {
+        highest = n_len;
+    }
 
-// /*
-//     Searches the input string `haystack` for the first instance of
-//     the string `needle`. This function returns a pointer that points
-//     to the first instance of the string `needle` in the input
-//     string `haystack`. 
+    for (int i = 0; i < highest; i++) 
+    {
+        int m_char = m[i] - '0';
+        int n_char = n[i] - '0';
 
-//     Do not use the `strstr` function from the standard library.
-// */
-// char *find_string(char *haystack, char *needle)
-// {
+        if (m_char < n_char)
+        {
+            return -1;
+        }
+        else if (m_char > n_char)
+        {
+            return 1;
+        }
+        else
+        {
+            continue;
+        }
+    }
 
-// }
+    return 0;
+}
+
+/*
+    Searches the input string `haystack` for the first instance of
+    the string `needle`. This function returns a pointer that points
+    to the first instance of the string `needle` in the input
+    string `haystack`. 
+
+    Do not use the `strstr` function from the standard library.
+*/
+char *find_string(char *haystack, char *needle)
+{
+    int haystack_len = strlen(haystack);
+    int needle_len = strlen(needle);
+    int counter = strlen(haystack);
+
+    for (int i = 0; i < haystack_len; i++)
+    {
+        if (haystack[i] == needle[0] && counter >= needle_len)
+        {
+            for (int x = 0; x < needle_len; x++)
+            {
+                if (haystack[i] == needle[x])
+                {
+                    return &haystack[i];
+                }
+            }
+        }
+        counter--;
+    }
+    return NULL;
+}
+
+// LambdaSchool
+// Lambda
 
 #ifndef TESTING
 int main(void)
@@ -91,14 +155,11 @@ int main(void)
     string_copy(buffer, hello);
 
     printf("Buffer is %s\n", buffer);
-    // printf("Comparison is %d\n", string_compare(hello, world));
-
-    // char *found_char = find_char(hello, 'e');
-    // char *found_string = find_string(world, "or");
-
-    // printf("Found char: %s\n", found_char);
-    // printf("Found string: %s\n", found_string);
-
+    printf("Comparison is %d\n", string_compare(hello, world));
+    char *found_char = find_char(hello, 'e');
+    char *found_string = find_string(world, "or");
+    printf("Found char: %s\n", found_char);
+    printf("Found string: %s\n", found_string);
     return 0;
 }
 #endif
--- pointers/tests/pointers_tests.c
+++ pointers/tests/pointers_tests.c
@@ -30,53 +30,53 @@ char *test_string_copy()
     return NULL;
 }
 
-// char *test_string_compare()
-// {
-//     char *s = "hello, world";
-//     char *t = "hiya";
-//     char *m = "goodbye";
-//     char *n = "see ya";
+char *test_string_compare()
+{
+    char *s = "hello, world";
+    char *t = "hiya";
+    char *m = "goodbye";
+    char *n = "see ya";
 
-//     char *firetruck = "firetruck";
-//     char *firetrucks = "firetrucks";
+    char *firetruck = "firetruck";
+    char *firetrucks = "firetrucks";
 
-//     mu_assert(string_compare(s, s) == 0, "Your string_compare did not return 0 for matching strings.");
-//     mu_assert(string_compare(s, t) < 0, "Your string_compare did not return a negative value where it was expected.");
-//     mu_assert(string_compare(n, m) > 0, "Your string_compare did not return a positive value where it was expected.");
-//     mu_assert(string_compare(firetruck, firetrucks) < 0, "Your string_compare did not return a negative value where it was expected.");
+    mu_assert(string_compare(s, s) == 0, "Your string_compare did not return 0 for matching strings.");
+    mu_assert(string_compare(s, t) < 0, "Your string_compare did not return a negative value where it was expected.");
+    mu_assert(string_compare(n, m) > 0, "Your string_compare did not return a positive value where it was expected.");
+    mu_assert(string_compare(firetruck, firetrucks) < 0, "Your string_compare did not return a negative value where it was expected.");
 
-//     return NULL;
-// }
+    return NULL;
+}
 
-// char *test_find_char()
-// {
-//     char *str = "LambdaSchool";
-//     char *found = find_char(str, 'b');
-//     mu_assert(string_compare(found, "bdaSchool") == 0, "Your find_char function did not work as expected.");
+char *test_find_char()
+{
+    char *str = "LambdaSchool";
+    char *found = find_char(str, 'b');
+    mu_assert(string_compare(found, "bdaSchool") == 0, "Your find_char function did not work as expected.");
 
-//     found = find_char(str, 'S');
-//     mu_assert(string_compare(found, "School") == 0, "Your find_char function did not work as expected.");
+    found = find_char(str, 'S');
+    mu_assert(string_compare(found, "School") == 0, "Your find_char function did not work as expected.");
 
-//     found = find_char(str, 's');
-//     mu_assert(!found, "Your find_char function return a char when it should have returned NULL.");
+    found = find_char(str, 's');
+    mu_assert(!found, "Your find_char function return a char when it should have returned NULL.");
 
-//     return NULL;
-// }
+    return NULL;
+}
 
-// char *test_find_string()
-// {
-//     char *str = "LambdaSchool";
-//     char *found = find_string(str, "School");
-//     mu_assert(string_compare(found, "School") == 0, "Your find_string function did not find the expected substring.");
+char *test_find_string()
+{
+    char *str = "LambdaSchool";
+    char *found = find_string(str, "School");
+    mu_assert(string_compare(found, "School") == 0, "Your find_string function did not find the expected substring.");
     
-//     found = find_string(str, "Lambda");
-//     mu_assert(string_compare(found, "LambdaSchool") == 0, "Your find_sting function did not find the expected substring.");
+    found = find_string(str, "Lambda");
+    mu_assert(string_compare(found, "LambdaSchool") == 0, "Your find_sting function did not find the expected substring.");
 
-//     found = find_string(str, "lambda");
-//     mu_assert(!found, "Your find_string function found a substring when it should have returned NULL.");
+    found = find_string(str, "lambda");
+    mu_assert(!found, "Your find_string function found a substring when it should have returned NULL.");
 
-//     return NULL;
-// }
+    return NULL;
+}
 
 char *all_tests()
 {
@@ -84,9 +84,9 @@ char *all_tests()
 
     mu_run_test(test_swap);
     mu_run_test(test_string_copy);
-    // mu_run_test(test_string_compare);
-    // mu_run_test(test_find_char);
-    // mu_run_test(test_find_string);
+    mu_run_test(test_string_compare);
+    mu_run_test(test_find_char);
+    mu_run_test(test_find_string);  
 
     return NULL;
 }
