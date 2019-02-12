3db827f27307c06bd88e403fa932caf493b3b0e5
Sean Chen <skypemaster007@gmail.com>
2019-02-12 11:23:35 -0800

    Move some pointers exercises over to Guided Project repo

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -1,17 +1,6 @@
 #include <stdio.h>
 
 /*
-    Swaps the integer values being pointed at by a and b. Keep in
-    mind when you need to access a pointer's actual value (the 
-    address it's referring to) or the value at the address it's 
-    pointing at.
-*/
-void swap(int* a, int* b)
-{
-
-}
-
-/*
     Searches the input string `str` for the first instance of the 
     character `c` (an unsigned char). This function returns a pointer
     that points to the first instance of the character `c` in the
@@ -20,38 +9,6 @@ void swap(int* a, int* b)
     Do not use the `strchr` function from the standard library.
 */
 char *find_char(char *str, int c)
-{
-
-}
-
-/*
-    Given an empty (NULL) character pointer x and a character pointer y,
-    copies the character contents of y over to x. Again, pointer arithmetic
-    is necessary here. Also, make sure x points to a null character at its 
-    end to terminate it properly. 
-    
-    Do not just use the `strcpy` function from the standard library.
-*/
-void string_copy(char *x, char *y)
-{
-
-}
-
-/* 
-    Compares the character strings m and n and returns negative,
-    0, or positive if n is lexicographically less than, equal to,
-    or greater than n. To calculate lexicographic difference, find
-    the difference between the first characters in m and n that differ.
-    
-    For example, given matching strings, this function should 
-    return 0. Given strings m = "hello world" and n = "goodbye",
-    this function should return a positive value. Given strings
-    m = "aardvark" and n = "zebra", should return a negative
-    value.
-    
-    Do not just use the `strcmp` function from the standard library.
-*/
-int string_compare(char *m, char *n)
 {
 
 }
@@ -72,19 +29,6 @@ char *find_string(char *haystack, char *needle)
 #ifndef TESTING
 int main(void)
 {
-    int x = 10, y = 20;
-    swap(&x, &y);
-    printf("x=%d, y=%d\n", x, y);
-
-    char *hello = "Hello";
-    char *world = "World";
-    char buffer[1024];
-
-    string_copy(buffer, hello);
-
-    printf("Buffer is %s\n", buffer);
-    printf("Comparison is %d\n", string_compare(hello, world));
-
     char *found_char = find_char(hello, 'e');
     char *found_string = find_string(world, "or");
 
--- pointers/tests/pointers_tests.c
+++ pointers/tests/pointers_tests.c
@@ -3,52 +3,6 @@
 #include "../../utils/minunit.h"
 #include "../../utils/utils.h"
 
-char *test_swap()
-{
-    int ints_short[] = { 4, 9 };
-    swap(&ints_short[0], &ints_short[1]);
-    mu_assert((ints_short[0] == 9 && ints_short[1] == 4), "Your swap function did not swap values correctly.");
-
-    int ints_long[] = { 1, 9, 2, 8, 3, 7, 6, 4, 5 };
-    swap(&ints_long[0], &ints_long[8]);
-    swap(&ints_long[3], &ints_long[4]);
-    mu_assert((ints_long[0] == 5 && ints_long[8] == 1), "Your swap function did not swap values correctly.");
-    mu_assert((ints_long[3] == 3 && ints_long[4] == 8), "Your swap function did not swap values correctly.");
-
-    mu_assert((ints_long[5] == 7 && ints_long[6] == 6), "Your swap function swapped values it shouldn't have.");
-
-    return NULL;
-}
-
-char *test_string_copy()
-{
-    char *string = "hello, world";
-    char empty[20];
-
-    string_copy(empty, string);
-    mu_assert(check_strings(empty, string) == 0, "Your string_copy did not correctly copy the given string.");
-
-    return NULL;
-}
-
-char *test_string_compare()
-{
-    char *s = "hello, world";
-    char *t = "hiya";
-    char *m = "goodbye";
-    char *n = "see ya";
-
-    char *firetruck = "firetruck";
-    char *firetrucks = "firetrucks";
-
-    mu_assert(string_compare(s, s) == 0, "Your string_compare did not return 0 for matching strings.");
-    mu_assert(string_compare(s, t) < 0, "Your string_compare did not return a negative value where it was expected.");
-    mu_assert(string_compare(n, m) > 0, "Your string_compare did not return a positive value where it was expected.");
-    mu_assert(string_compare(firetruck, firetrucks) < 0, "Your string_compare did not return a negative value where it was expected.");
-
-    return NULL;
-}
-
 char *test_find_char()
 {
     char *str = "LambdaSchool";
@@ -89,9 +43,6 @@ char *all_tests()
 {
     mu_suite_start();
 
-    mu_run_test(test_swap);
-    mu_run_test(test_string_copy);
-    mu_run_test(test_string_compare);
     mu_run_test(test_find_char);
     mu_run_test(test_find_string);
 
