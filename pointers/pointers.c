d1fee08169e0772a9a34e21db7795c3992b8b43f
jeanfern5 <jeanfern555@gmail.com>
2019-02-12 14:23:29 -0600

    Added instructor's pseudo code and notes from class.

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
 /*
+    Swaps the integer values being pointed at by a and b. Keep in
+    mind when you need to access a pointer's actual value (the 
+    address it's referring to) or the value at the address it's 
+    pointing at.
+*/
+void swap(int* a, int* b)
+{ 
+    // int *temp = 0;
+    //     temp = *a;
+    //     *a = *b
+    //     *b = temp
+
+    // return 0;
+}
+
+/*
     Searches the input string `str` for the first instance of the 
     character `c` (an unsigned char). This function returns a pointer
     that points to the first instance of the character `c` in the
@@ -14,6 +30,58 @@ char *find_char(char *str, int c)
 }
 
 /*
+    Given an empty (NULL) character pointer x and a character pointer y,
+    copies the character contents of y over to x. Again, pointer arithmetic
+    is necessary here. Also, make sure x points to a null character at its 
+    end to terminate it properly. 
+    
+    Do not just use the `strcpy` function from the standard library.
+*/
+void string_copy(char *x, char *y)
+{
+    //iterate along y char-by-char
+    //set the corresponding in x to that char
+
+    // while(*y != '\0') {
+    //     *x = *y;
+    //     x++;
+    //     y++;
+    // };
+    
+    // *x = "\0";
+};
+
+/* 
+    Compares the character strings m and n and returns negative,
+    0, or positive if n is lexicographically less than, equal to,
+    or greater than n. To calculate lexicographic difference, find
+    the difference between the first characters in m and n that differ.
+    
+    For example, given matching strings, this function should 
+    return 0. Given strings m = "hello world" and n = "goodbye",
+    this function should return a positive value. Given strings
+    m = "aardvark" and n = "zebra", should return a negative
+    value.
+    
+    Do not just use the `strcmp` function from the standard library.
+*/
+int string_compare(char *m, char *n)
+{
+    // //iterate along both strings
+    // //as long as each corresponding char matches
+    // for (; *m == *n; m++, n++)
+    // {
+    //     if (*m == '\0') 
+    //     {
+    //         return 0;
+    //     }
+    // };
+
+    // //if we find a mismatch
+    // return *m-*n;
+};
+
+/*
     Searches the input string `haystack` for the first instance of
     the string `needle`. This function returns a pointer that points
     to the first instance of the string `needle` in the input
@@ -23,12 +91,33 @@ char *find_char(char *str, int c)
 */
 char *find_string(char *haystack, char *needle)
 {
+    //iterate along haystack until '\0'
+        //check to see if current haystack char matches 
+        //the first char of needle
+        //if it does, start iterating along needle as well
+        //check if each char matches
+
+    //what happens when not all the needle char match?
+    //how do we continue iterating in that case?
 
 }
 
 #ifndef TESTING
 int main(void)
 {
+    int x = 10, y = 20;
+    swap(&x, &y);
+    printf("x=%d, y=%d\n", x, y);
+
+    char *hello = "Hello";
+    char *world = "World";
+    char buffer[1024];
+
+    string_copy(buffer, hello);
+
+    printf("Buffer is %s\n", buffer);
+    printf("Comparison is %d\n", string_compare(hello, world));
+
     char *found_char = find_char(hello, 'e');
     char *found_string = find_string(world, "or");
 
