04bbee5c2c3fa4694b84d6c8b9741f52e1304a64
ckirkwood94 <ckirkwood94@gmail.com>
2019-02-14 18:44:12 -0500

    quicksort complete

--- quicksort/quicksort.c
+++ quicksort/quicksort.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "lib.h" 
+#include "lib.h"
 
 /*
     Implement the Quicksort algorithm. You'll likely want to re-use the
@@ -17,7 +17,30 @@
 */
 void quicksort(int *arr, int low, int high)
 {
+    if (low < high)
+    {
+        int pivot = partition(arr, low, high);
 
+        quicksort(arr, low, pivot - 1);
+        quicksort(arr, pivot + 1, high);
+    }
+}
+
+int partition(int *arr, int low, int high)
+{
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
 }
 
 #ifndef TESTING
@@ -25,9 +48,10 @@ int main(void)
 {
     int arr1[] = {100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79};
     int n = sizeof(arr1) / sizeof(arr1[0]);
-    quicksort(arr1, 0, n-1);
+    quicksort(arr1, 0, n - 1);
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         printf("%d ", arr1[i]);
     }
 
