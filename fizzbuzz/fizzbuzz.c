f7dc49f0990ae1385d8a3007f2020af3dca8d148
txiong000 <tkx422@gmail.com>
2019-02-18 15:02:21 -0600

    fizzbuzz

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,21 @@
 */
 int fizzbuzz(int n)
 {
-
+    int counter;
+    while (n > counter) {
+        if n % 3 == 0 {
+            printf("Fizz");
+        }
+        else if n % 5 == 0 {
+            printf("Buzz");
+        }
+        else if n % 3 == 0 && n % 5 == 0 {
+            printf("FizzBuzz");
+        }
+        else {
+            counter++
+        }
+    }
 }
 
 #ifndef TESTING
