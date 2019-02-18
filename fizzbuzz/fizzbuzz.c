864de93f3177acab823066ae6eedef1a1f41ec75
Scott Bren <smbren@ncsu.edu>
2019-02-18 16:18:34 -0500

    initial commit

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -6,7 +6,7 @@
     "Fizz". If it's divisible by 5, print "Buzz". If it's divisible
     by both 3 and 5, print "FizzBuzz". Increment the counter variable
     every time that nothing gets printed and return the counter.
-    Don't forget to include newlines '\n' in your printf statements!
+    Don't forget to include newlines '\n' in your printf statements! -
 */
 int fizzbuzz(int n)
 {
