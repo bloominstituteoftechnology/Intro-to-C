5b27b8fd26b0ef45afc2fb803b269508bbbce54d
michaelagard <mdagard@gmail.com>
2019-02-18 18:40:18 -0500

    refactored statement to print correctly

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -13,17 +13,18 @@ int fizzbuzz(int n)
   int counter = 0;
   for (int i = 0; i < n; i++)
   {
-    if (i % 3 == 0)
+    printf("%d\n", i);
+    if (i % 3 == 0 && i % 5 == 0)
     {
       printf("FizzBuzz\n");
-    }
-    else if (i % 5 == 0)
-    {
-      printf("Fizz\n");
     }
     else if (i % 3 == 0)
     {
       printf("Buzz\n");
+    }
+    else if (i % 5 == 0)
+    {
+      printf("Fizz\n");
     }
     else
     {
