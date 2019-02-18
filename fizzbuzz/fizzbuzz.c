87e94c449ffa9c72d52f6b467db86b7d34b17297
luiscmartinez <mrluismartinezzz@gmail.com>
2019-02-18 08:24:55 -0800

    completed and passed all fizzbuzz test

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,25 @@
 */
 int fizzbuzz(int n)
 {
-
+int i;
+int counter = 0;
+  for( i = 1; i <= n; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      printf("FizzBuzz");
+    }
+    if (i % 3 == 0 || i % 5 == 0) {
+      if (i % 3 == 0) {
+        printf("Fizz");
+      }
+      if (i % 5 == 0) {
+        printf("Buzz");
+      }
+    } else {
+      counter++;
+    }
+  }
+    printf("%i\n", counter);
+    return counter;
 }
 
 #ifndef TESTING
