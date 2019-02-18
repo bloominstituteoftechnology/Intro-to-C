b653b3db7170ca596be91346397675afed0aa94a
Scott Bren <smbren@ncsu.edu>
2019-02-18 17:08:28 -0500

    fizzbuzz complete, all tests pass

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,6 +10,25 @@
 */
 int fizzbuzz(int n)
 {
+    //initialize counter
+    int i;
+    int count = 0;
+
+    for( i = 0; i < n; i++) {
+
+        if( (i % 5 == 0) && (i % 3 == 0) ) {
+            printf("FizzBuzz\n");
+        } else if ( i % 5 == 0 ){  
+            printf("Buzz\n");
+        } else if ( i % 3 == 0 ) {
+            printf("Fizz\n");
+        } else {
+            count++;
+        }
+
+    }
+    
+    return count;
 
 }
 
