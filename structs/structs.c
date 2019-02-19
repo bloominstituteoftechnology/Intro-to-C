24a1866ba2d86910b4063bd4bef6c734593f4c3a
Farhan Farooqui <farhan@farooqui.net>
2019-02-19 12:44:06 -0500

    structs complete

--- structs/structs.c
+++ structs/structs.c
@@ -8,7 +8,10 @@
     Person should have the fields `name`, `age`, `height`, and `weight`.
 */
 typedef struct Person {
-
+    char *name;
+    int age;
+    int height;
+    int weight;
 } Person;
 
 /*
@@ -21,7 +24,13 @@ typedef struct Person {
 */
 Person *createPerson(char *name, int age, int height, int weight)
 {
+    Person* person = malloc(sizeof(Person));
+    person->name = string_dup(name);
+    person->age = age;
+    person->height = height;
+    person->weight = weight;
 
+    return person;
 }
 
 /*
@@ -30,7 +39,8 @@ Person *createPerson(char *name, int age, int height, int weight)
 */
 void destroyPerson(Person *who)
 {
-
+    free(who->name);
+    free(who);
 }
 
 #ifndef TESTING
