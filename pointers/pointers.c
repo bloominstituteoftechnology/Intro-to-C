efdaea16fc2cb000af7a04ecdfdfe05d6cc7b780
IAN CAMERON <iancameron@IANs-MacBook-Pro.local>
2019-02-19 16:04:26 -0700

    adds comments

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -10,10 +10,13 @@ void string_copy(char *x, char *y)
 {
     while(*y != '\0')
     {
+        //copying contents of y over to x
         *y = *x;
+        //iterate through both
         x++;
         y++;
     }
+    //terminating properly with x pointing to null terminator
     *x = '\0';
 }
 
@@ -27,14 +30,19 @@ void string_copy(char *x, char *y)
 */
 char *find_char(char *str, int c)
 {
+    //while str is not null
     while (*str != '\0') 
     {
+        //loop through checking every character against c, 
         if (*str == c)
         {
-            return (str);
+            //if find the char you're looking for return
+            return (str);  //why return str for c? 
         }
+        //iterate through str
         str++;
     }
+    //better than returning 0 since supposed to return pointer
     return NULL;
 }
 
