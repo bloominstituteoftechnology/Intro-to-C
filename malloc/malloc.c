212b248215ee97ad5e9add8b9987dba63ff4ddcb
Riley-Brown <riley@riley.gg>
2019-02-21 19:04:50 -0500

    Added code to duplicate string using malloc

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -12,7 +12,14 @@
 */
 char *string_dup(char *src)
 {
+    char *d = malloc(strlen(src) + 1);
+    if (d == NULL)
+    {
+        return NULL;
+    }
 
+    strcpy(d, src);
+    return d;
 }
 
 /*
@@ -24,7 +31,6 @@ char *string_dup(char *src)
 */
 void mem_copy(void *dest, const void *src, int n)
 {
-
 }
 
 /*
@@ -40,7 +46,6 @@ void mem_copy(void *dest, const void *src, int n)
 */
 void *resize_memory(void *ptr, int old_size, int new_size)
 {
-
 }
 
 #ifndef TESTING
@@ -54,12 +59,13 @@ int main(void)
     int numbers[] = {100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79};
     int n = sizeof(numbers) / sizeof(numbers[0]);
     int *target = malloc(n * sizeof(int));
-    
+
     mem_copy(target, numbers, n * sizeof(int));
 
     printf("Copied array: ");
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         printf("%d ", target[i]);
     }
 
@@ -69,12 +75,13 @@ int main(void)
     char *path = string_dup("/students/");
     int url_length = string_length(url);
     int path_length = string_length(path);
-    
+
     int new_length = url_length - 1 + path_length;
     char *new_url = resize_memory(url, url_length, new_length);
     char *p = new_url + url_length;
 
-    while (*path != '\0') {
+    while (*path != '\0')
+    {
         *p = *path;
         p++;
         path++;
