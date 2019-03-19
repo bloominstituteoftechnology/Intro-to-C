c3a6e77113c16e334153d635d559aab2dd307752
Barbara Hunter <commandandcode@gmail.com>
2019-03-19 18:56:20 -0400

    working on pointer bug

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -8,7 +8,14 @@
 */
 void string_copy(char *x, char *y)
 {
-
+        while (*y != '\0') 
+    {
+        //copy x to y
+        *x = *y;
+        // inc x and y
+        x++;
+        y++;
+    }
 }
 
 /*
@@ -21,7 +28,19 @@ void string_copy(char *x, char *y)
 */
 char *find_char(char *str, int c)
 {
-
+    //while there is a string
+    while (*str != '\0')
+  {
+      //if string = c
+    if (*str == c)
+    {
+        //return
+      return str;
+    }
+    //inc
+    str++;
+  }
+  return NULL;
 }
 
 /*
@@ -34,12 +53,23 @@ char *find_char(char *str, int c)
 */
 char *find_string(char *haystack, char *needle)
 {
+    char *start = find_char(haystack, needle[0]);
+    for (int i = 0; needle[i] != '\0'; i++)
+  {
+    if (start[i] != needle[i])
+    {
+      return NULL;
+    }
+  }
+  return start;
 
 }
 
 #ifndef TESTING
 int main(void)
-{
+{   
+    char *hello = "hello";
+    char *world = "world";
     char *found_char = find_char(hello, 'e');
     char *found_string = find_string(world, "or");
 
