47511a6f042199c79db349bbd0705a7cc0e84df8
jeffyjkang <jeffyjkang@gmail.com>
2019-03-19 16:04:28 -0700

    start of malloc

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -12,7 +12,24 @@
 */
 char *string_dup(char *src)
 {
-
+    // get length of src
+    int length = strlen(src);
+    // printf("%d", string_length);
+    // assign duplicate to malloc with length
+    char *copy = malloc(length);
+    //while deref src does not equal null char
+    while (*src != '\0')
+    {
+        // assign deref duplicate to deref src
+        *copy = *src;
+        // increment duplicate
+        copy++;
+        // increment src
+        src++;
+    };
+    // duplicate now in correct position should have null char
+    *copy = '\0';
+    return copy;
 }
 
 /*
@@ -24,7 +41,6 @@ char *string_dup(char *src)
 */
 void mem_copy(void *dest, const void *src, int n)
 {
-
 }
 
 /*
@@ -40,10 +56,9 @@ void mem_copy(void *dest, const void *src, int n)
 */
 void *resize_memory(void *ptr, int old_size, int new_size)
 {
-
 }
 
-#ifndef TESTING
+// #ifndef TESTING
 int main(void)
 {
     char *s = "Some string to duplicate.";
@@ -54,12 +69,13 @@ int main(void)
     int numbers[] = {100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79};
     int n = sizeof(numbers) / sizeof(numbers[0]);
     int *target = malloc(n * sizeof(int));
-    
+
     mem_copy(target, numbers, n * sizeof(int));
 
     printf("Copied array: ");
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         printf("%d ", target[i]);
     }
 
@@ -69,12 +85,13 @@ int main(void)
     char *path = string_dup("/students/");
     int url_length = string_length(url);
     int path_length = string_length(path);
-    
+
     int new_length = url_length - 1 + path_length;
     char *new_url = resize_memory(url, url_length, new_length);
     char *p = new_url + url_length;
 
-    while (*path != '\0') {
+    while (*path != '\0')
+    {
         *p = *path;
         p++;
         path++;
@@ -84,4 +101,4 @@ int main(void)
 
     return 0;
 }
-#endif
+// #endif
