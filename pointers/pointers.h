fa67326469806d47790051111cb55cd227a52135
johnoro <officialjohnorourke@gmail.com>
2019-05-24 16:44:18 -0400

    Removes unused headers in pointers

--- pointers/pointers.h
+++ pointers/pointers.h
@@ -1,13 +1,7 @@
 #ifndef pointers_h
 #define pointers_h
 
-void swap(int *a, int *b);
-
-int string_length(char *s);
-
 void string_copy(char *a, char *b);
-
-int string_compare(char *m, char *n);
 
 char *find_char(char *str, int c);
 
