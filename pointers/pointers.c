20597096cd2051772d38735269b58d8bc11d7797
johnoro <officialjohnorourke@gmail.com>
2019-05-24 09:58:22 -0400

    Fixes strings in pointers main function

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -47,8 +47,8 @@ char *find_string(char *haystack, char *needle)
 #ifndef TESTING
 int main(void)
 {
-    char *found_char = find_char(hello, 'e');
-    char *found_string = find_string(world, "or");
+    char *found_char = find_char("hello", 'e');
+    char *found_string = find_string("world", "or");
 
     printf("Found char: %s\n", found_char);
     printf("Found string: %s\n", found_string);
